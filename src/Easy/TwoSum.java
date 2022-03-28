512eae3c314bd6f007e7bbe11eb3811dcf78c228
SANAYEE <dudtkd0219@gmail.com>
2022-03-28 15:49:09 +0900

    2.Two Sum

---
+++ src/Easy/TwoSum.java
@@ -0,0 +1,14 @@
+package Easy;
+
+public class TwoSum {
+	public static int[] twoSum(int[] nums, int target) {
+        
+		return nums;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

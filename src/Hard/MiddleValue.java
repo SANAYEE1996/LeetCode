d295eb672a4435f02957b9640cc90fb71f583cd2
SANAYEE <dudtkd0219@gmail.com>
2022-04-10 19:28:33 +0900

    중간값 구하기 근데 너무 linear 함

--- src/Hard/MiddleValue.java
+++ src/Hard/MiddleValue.java
@@ -1,13 +1,38 @@
 package Hard;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class MiddleValue {
-	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         
-        return 0;
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		for(int i : nums1) {
+			list.add(i);
+		}
+		
+		for(int i : nums2) {
+			list.add(i);
+		}
+		
+		Collections.sort(list);
+		
+		if(list.size() % 2 == 0) {
+			return (double)(list.get(list.size()/2)+list.get(list.size()/2 - 1))/2;
+		}
+		else {
+			return (double)list.get(list.size()/2);
+		}
     }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		MiddleValue s = new MiddleValue();
+		
+		int[] a = {1,2};
+		int[] b = {3,4};
+		
+		System.out.println("the answer is : " +s.findMedianSortedArrays(a, b));
 
 	}
 

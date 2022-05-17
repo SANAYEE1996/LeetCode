c7b225d57210848568a2488f7e1e58d88c1921f2
SANAYEE <dudtkd0219@gmail.com>
2022-05-17 18:49:51 +0900

    ?

--- src/Hard/MergeKList.java
+++ src/Hard/MergeKList.java
@@ -1,6 +1,5 @@
 package Hard;
 
-import java.util.Arrays;
 
 public class MergeKList {
 	public ListNode mergeKLists(ListNode[] lists) {
@@ -11,27 +10,12 @@ public class MergeKList {
 
 	public static void main(String[] args) {
 		
-		ListNode a = new ListNode(1);
-		ListNode b = new ListNode(3);
-		ListNode c = new ListNode(5);
+		ListNode a = new ListNode(100);
 		
 		
-		a.next = new ListNode(2);
-		b.next = new ListNode(4);
-		c.next = new ListNode(6);
 		
-		ListNode[] lists = {a,b,c};
 		
-		System.out.println(lists[0].val);
-		System.out.println(lists[0].next.val);
-		System.out.println(Arrays.toString(lists));
-		for(int i = 0; i < lists.length; i++) {
-			System.out.println(lists[i].val);
-			while(lists[i].next.equals(null)) {
-				
-				System.out.println(lists[i].next.val);
-			}
-		}
+		
 
 	}
 

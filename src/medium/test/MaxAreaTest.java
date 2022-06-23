872281b9a0ac2906321cf0c2ba6939c843a4d2a6
SANAYEE <dudtkd0219@gmail.com>
2022-06-24 08:57:34 +0900

    maxArea Test

---
+++ src/medium/test/MaxAreaTest.java
@@ -0,0 +1,24 @@
+package medium.test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import medium.MaxArea;
+
+class MaxAreaTest {
+	
+	private MaxArea area;
+	
+	@BeforeEach
+	public void setup() {
+		area = new MaxArea();
+	}
+	
+	@Test
+	void testCase01() {
+		fail("Not yet implemented");
+	}
+
+}

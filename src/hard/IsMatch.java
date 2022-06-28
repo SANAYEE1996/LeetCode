b759daa6dfcf3222647b17980683151c38ed8266
SANAYEE <dudtkd0219@gmail.com>
2022-06-28 12:58:19 +0900

    집가서 할 예정 다 함

--- src/hard/IsMatch.java
+++ src/hard/IsMatch.java
@@ -2,47 +2,46 @@ package hard;
 
 public class IsMatch {
 	public boolean isMatch(String s, String p) {
-		if(isAllStar(p) || s.equals(p)) return true;
 		if(!p.contains("*") && (p.length() != s.length())) return false;
-		int[] sIndex = {0};
+		int sIndex = 0;
 		char nowChar = 0;
-		char imshi = 0;
+		int index = 0;
+		String star = "";
 		
 		for(int i = 0; i < p.length(); i++) {
 			nowChar = p.charAt(i);
 			if(nowChar == '?') {
-				sIndex[0] = (s.length()-1 > sIndex[0]) ? sIndex[0]++:sIndex[0];
+				sIndex = (sIndex + 1 <= s.length()-1) ? sIndex+1 : s.length()-1;
+				continue;
 			}
 			else if(nowChar == '*') {
-				i++;
-				if(i == p.length()) return true;
-				imshi = p.charAt(i);
-				if(!pass(s, imshi, sIndex)) return false;
+				star = starString(p,i+1);
+				if(star.equals("")) return true;
+				i += star.length();
+				index = s.lastIndexOf(star);
+				if(index < 0) {
+					return false;
+				}
+				sIndex = (index + star.length() <= s.length()-1) ? index + star.length() : s.length()-1;
+				continue;
 			}
-			else if(nowChar != s.charAt(sIndex[0]))	return false;
-			sIndex[0] = (s.length()-1 > sIndex[0]) ? sIndex[0]++:sIndex[0];
+			else if(nowChar != s.charAt(sIndex)) {
+				return false;
+			}
+			sIndex = (sIndex + 1 <= s.length()-1) ? sIndex+1 : s.length()-1;
 		}
 		
 		return true;
     }
 	
-	public boolean isAllStar(String p) {
-		for(int i = 0; i < p.length(); i++) {
-			if(p.charAt(i) != '*') {
-				return false;
-			}
-		}
-		return true;
-	}
 	
-	public boolean pass(String s, char imshi, int[] sIndex) {
-		int start = sIndex[0];
-		for(int i = start; i < s.length(); i++) {
-			sIndex[0] = (s.length()-1 > sIndex[0]) ? sIndex[0]++:sIndex[0];
-			if(s.charAt(i) == imshi) {
-				return true;
+	public String starString(String p, int i) {
+		int start = i;
+		for(; i < p.length(); i++) {
+			if(p.charAt(i) == '?' || p.charAt(i) == '*' ) {
+				return p.substring(start,i);
 			}
 		}
-		return false;
+		return "";
 	}
 }

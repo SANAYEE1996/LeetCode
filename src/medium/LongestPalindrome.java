f58fa9aa0996b95bdbd3e6903341d697cb998e75
SANAYEE <dudtkd0219@gmail.com>
2022-07-01 01:12:27 +0900

    가장 긴 팰린드롬 완성

--- src/medium/LongestPalindrome.java
+++ src/medium/LongestPalindrome.java
@@ -1,37 +1,47 @@
 package medium;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class LongestPalindrome {
 	
 	public String longestPalindrome(String s) {
-		int end = s.length()/2;
+//		System.out.println("input string : " +s);
+		int end = s.length();
 		int lastIndex = s.length()-1;
 		ArrayList<String> list = new ArrayList<>();
-		for(int i = 0; i <= end; i++) {
-			if(s.charAt(i) == s.charAt(i+1)) {
+		for(int i = 0; i < end; i++) {
+			if(i < lastIndex && s.charAt(i) == s.charAt(i+1)) {
+				list.add(s.substring(i, i+2));
 				even(s, i, lastIndex, list);
 			}
-			if(i > 0 && (s.charAt(i-1) == s.charAt(i+1))) {
+			if(i > 0 && i < lastIndex && (s.charAt(i-1) == s.charAt(i+1))) {
+				list.add(s.substring(i-1, i+2));
 				odd(s, i, lastIndex, list);
 			}
 		}
+		if(list.size() == 0) return s.substring(0, 1);
+		
+		Collections.sort(list, new Comparator<String>() {
+	        @Override
+	        public int compare(String s1, String s2) {
+	            return s2.length()-s1.length();
+	        }
+	    });
 		System.out.println(list);
-		return "";
+		return list.get(0);
     }
 	
 	public void even(String s, int index, int lastIndex, ArrayList<String> list) {
 		int i = (index-1 >= 0) ? index-1 : 0;
 		int j = (index+2 <= lastIndex) ? index+2 : lastIndex;
-		System.out.println(index);
 		if(s.charAt(i) == s.charAt(j)) {
 			while(s.charAt(i) == s.charAt(j)){
-				System.out.println("i : " + i + " // j : " +j);
 				i--;
 				j++;
-				if((i < 0 || j > lastIndex)) {
+				if((i < 0 || j > lastIndex)|| (s.charAt(i) != s.charAt(j))) {
 					i += 1;
-					j -= 1;
 					break;
 				}
 			}
@@ -40,15 +50,14 @@ public class LongestPalindrome {
 	}
 	
 	public void odd(String s, int index, int lastIndex, ArrayList<String> list) {
-		int i = index-1;
-		int j = (index+1 <= lastIndex) ? index+1 : lastIndex;
+		int i = (index-2 >= 0) ? index-2 : 0;
+		int j = (index+2 <= lastIndex) ? index+2 : lastIndex;
 		if(s.charAt(i) == s.charAt(j)) {
-			while(s.charAt(i) == s.charAt(j)){			
+			while(s.charAt(i) == s.charAt(j)){
 				i--;
 				j++;
-				if((i < 0 || j > lastIndex)) {
+				if((i < 0 || j > lastIndex) || (s.charAt(i) != s.charAt(j))) {
 					i += 1;
-					j -= 1;
 					break;
 				}
 			}

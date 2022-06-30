1b035727b74cd9809bb2bfd1db406a3466be21ed
SANAYEE <dudtkd0219@gmail.com>
2022-07-01 01:13:26 +0900

    Runtime: 82 ms faster than 42.90% // Memory 55.1 MB less than 30.60%

--- src/medium/test/LongestPalindromeTest.java
+++ src/medium/test/LongestPalindromeTest.java
@@ -18,8 +18,58 @@ class LongestPalindromeTest {
 	}
 	
 	@Test
-	void test() {
+	void test01() {
 		assertEquals("aba", p.longestPalindrome("aba"));
+	}
+	
+	@Test
+	void test02() {
+		assertEquals("a", p.longestPalindrome("a"));
+	}
+	
+	@Test
+	void test03() {
+		assertEquals("bb", p.longestPalindrome("abbd"));
+	}
+	
+	@Test
+	void test04() {
+		assertEquals("abba", p.longestPalindrome("abba"));
+	}
+	
+	@Test
+	void test05() {
+		assertEquals("aa", p.longestPalindrome("aa"));
+	}
+	
+	@Test
+	void test06() {
+		assertEquals("bab", p.longestPalindrome("babad"));
+	}
+	
+	@Test
+	void test07() {
+		assertEquals("bbbbbbb", p.longestPalindrome("bbbbbbba"));
+	}
+	
+	@Test
+	void test08() {
+		assertEquals("abbbbbbba", p.longestPalindrome("abbbbbbba"));
+	}
+	
+	@Test
+	void test09() {
+		assertEquals("a", p.longestPalindrome("abcdefgh"));
+	}
+	
+	@Test
+	void test10() {
+		assertEquals("bcb", p.longestPalindrome("eabcb"));
+	}
+	
+	@Test
+	void test11() {
+		assertEquals("ddtattarrattatdd", p.longestPalindrome("addtattarrattatdde"));
 	}
 
 }

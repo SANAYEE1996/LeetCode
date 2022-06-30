42796041638ad6c0984281748676d9a8a6838700
SANAYEE <dudtkd0219@gmail.com>
2022-07-01 01:28:14 +0900

    속도는 조금 빨라졌는데 메모리는 비슷비슷 어레이리스트 메모리 얼마 안차지하나?

--- src/medium/LongestPalindrome.java
+++ src/medium/LongestPalindrome.java
@@ -1,39 +1,32 @@
 package medium;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-
 public class LongestPalindrome {
 	
 	public String longestPalindrome(String s) {
 //		System.out.println("input string : " +s);
 		int end = s.length();
 		int lastIndex = s.length()-1;
-		ArrayList<String> list = new ArrayList<>();
+		String max = s.substring(0, 1);
+		String maybeMax = "";
 		for(int i = 0; i < end; i++) {
 			if(i < lastIndex && s.charAt(i) == s.charAt(i+1)) {
-				list.add(s.substring(i, i+2));
-				even(s, i, lastIndex, list);
+				maybeMax = s.substring(i, i+2);
+				max = (maybeMax.length() > max.length()) ? maybeMax : max;
+				maybeMax = even(s, i, lastIndex);
+				max = (maybeMax.length() > max.length()) ? maybeMax : max;
 			}
 			if(i > 0 && i < lastIndex && (s.charAt(i-1) == s.charAt(i+1))) {
-				list.add(s.substring(i-1, i+2));
-				odd(s, i, lastIndex, list);
+				maybeMax = s.substring(i-1, i+2);
+				max = (maybeMax.length() > max.length()) ? maybeMax : max;
+				maybeMax = odd(s, i, lastIndex);
+				max = (maybeMax.length() > max.length()) ? maybeMax : max;
 			}
 		}
-		if(list.size() == 0) return s.substring(0, 1);
-		
-		Collections.sort(list, new Comparator<String>() {
-	        @Override
-	        public int compare(String s1, String s2) {
-	            return s2.length()-s1.length();
-	        }
-	    });
-		System.out.println(list);
-		return list.get(0);
+		return max;
     }
 	
-	public void even(String s, int index, int lastIndex, ArrayList<String> list) {
+	
+	public String even(String s, int index, int lastIndex) {
 		int i = (index-1 >= 0) ? index-1 : 0;
 		int j = (index+2 <= lastIndex) ? index+2 : lastIndex;
 		if(s.charAt(i) == s.charAt(j)) {
@@ -45,11 +38,12 @@ public class LongestPalindrome {
 					break;
 				}
 			}
-			list.add(s.substring(i, j));
+			return s.substring(i,j);
 		}
+		return "";
 	}
 	
-	public void odd(String s, int index, int lastIndex, ArrayList<String> list) {
+	public String odd(String s, int index, int lastIndex) {
 		int i = (index-2 >= 0) ? index-2 : 0;
 		int j = (index+2 <= lastIndex) ? index+2 : lastIndex;
 		if(s.charAt(i) == s.charAt(j)) {
@@ -61,8 +55,9 @@ public class LongestPalindrome {
 					break;
 				}
 			}
-			list.add(s.substring(i, j));
+			return s.substring(i,j);
 		}
+		return "";
 	}
 	
 

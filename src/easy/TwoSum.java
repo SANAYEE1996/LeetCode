597826716dd8cec5ddd919d7f027fceca0f83332
SANAYEE <dudtkd0219@gmail.com>
2022-07-07 02:43:49 +0900

    초안

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -5,6 +5,22 @@ public class TwoSum {
          
 		return nums;
     }
+	
+	int BSearch(int arr[], int target, int lowIndex) {
+	    int high = arr.length - 1;
+	    int mid;
 
+	    while(lowIndex <= high) {
+	        mid = (lowIndex + high) / 2;
+
+	        if (arr[mid] == target)
+	            return mid;
+	        else if (arr[mid] > target)
+	            high = mid - 1;
+	        else
+	            lowIndex = mid + 1;
+	    }
+	    return -1;
+	}
 
 }

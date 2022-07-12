a34319b7d49c7e013574e22ca67f8fa7489de927
SANAYEE <dudtkd0219@gmail.com>
2022-07-12 17:49:35 +0900

    문자열 매칭 테스트

---
+++ src/hard/test/FindSubstringTest.java
@@ -0,0 +1,27 @@
+package hard.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import hard.FindSubstring;
+
+class FindSubstringTest {
+	
+	private FindSubstring fs;
+	
+	@BeforeEach
+	public void setup() {
+		fs = new FindSubstring();
+	}
+	
+	@Test
+	void test() {
+		System.out.println(fs.findSubstring("foobar", new String[] {"foo","bar"}));
+		System.out.println(fs.findSubstring("barfoothefoobarman", new String[] {"foo","bar"}));
+		System.out.println(fs.findSubstring("wordgoodgoodgoodbestword", new String[] {"word","good","best","word"}));
+		System.out.println(fs.findSubstring("barfoofoobarthefoobarman", new String[] {"bar","foo","the"}));
+		System.out.println(fs.findSubstring("", new String[] {""}));
+		System.out.println(fs.findSubstring("wordgoodgoodgoodbestword", new String[] {"word","good","best","good"}));
+	}
+
+}

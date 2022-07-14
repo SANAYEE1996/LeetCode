c2ad4e3b7ab2466f5ccdddc6a975f03dc534ea14
SANAYEE <dudtkd0219@gmail.com>
2022-07-14 14:36:50 +0900

    두개 합 구하기

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -1,10 +1,23 @@
 package easy;
 
 public class TwoSum {
-	public int[] twoSum(int[] nums, int target) {
-         
-		return nums;
-    }
+	 public int[] twoSum(int[] nums, int target) {
+	        int[] answer = new int[2];
+	        
+	        
+	        END:
+	        for(int i = 0; i < nums.length-1; i++){
+	            answer[0] = i;
+	            for(int j = i+1; j < nums.length; j++){
+	                if(nums[i]+nums[j] == target){
+	                    answer[1] = j;
+	                    break END;
+	                }
+	            }
+	        }
+	            
+	        return answer;
+	    }
 	
 	int BSearch(int arr[], int target, int lowIndex) {
 	    int high = arr.length - 1;

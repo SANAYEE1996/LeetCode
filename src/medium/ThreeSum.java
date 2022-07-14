927451131794d6bd5f94d512cf04f4173e96a89a
SANAYEE <dudtkd0219@gmail.com>
2022-07-15 00:53:40 +0900

    똥 싸는 중

--- src/medium/ThreeSum.java
+++ src/medium/ThreeSum.java
@@ -2,134 +2,115 @@ package medium;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class ThreeSum {
 	public List<List<Integer>> threeSum(int[] nums) {
+		System.out.println(nums.length);
 		List<List<Integer>> list = new ArrayList<>();
-		ArrayList<Integer> minusIndex = new ArrayList<>();
-		ArrayList<Integer> zeroIndex = new ArrayList<>();
-		ArrayList<Integer> plusIndex = new ArrayList<>();
+		HashMap<Integer, Integer> minusValue = new HashMap<>();
+		HashMap<Integer, Integer> plusValue = new HashMap<>();
+		ArrayList<Integer> minusList = new ArrayList<>();
+		ArrayList<Integer> plusList = new ArrayList<>();
 		Arrays.sort(nums);
-		for(int i = 0; i < nums.length; i++) {
-			if(nums[i] < 0) {minusIndex.add(i); continue;}
-			else if(nums[i] == 0) {zeroIndex.add(i); continue;}
-			plusIndex.add(i);
+		for(int i : nums) {
+			if(i > 0) {
+				plusValue.put(i,i);
+				plusList.add(i);
+			}
+			else if(i < 0) {
+				minusValue.put(i,i);
+				minusList.add(i);
+			}
 		}
-		System.out.println(Arrays.toString(nums));
-		System.out.println(minusIndex);
-		System.out.println(zeroIndex);
-		System.out.println(plusIndex);
-		if((minusIndex.size() == 0 || plusIndex.size() == 0) && zeroIndex.size() < 3) return list;
+		int plusCount = plusList.size();
+		int minusCount = minusList.size();
+		int zeroCount = nums.length-(plusCount + minusCount);
+		if((plusCount == 0 || minusCount == 0) && zeroCount < 3) return list;
 		
-		if(minusIndex.size() > 1 && plusIndex.size() > 0) 
-			twoMinusOnePlus(minusIndex, plusIndex, list, nums);
-		if(minusIndex.size() > 0 && plusIndex.size() > 1) 
-			oneMinusTwoPlus(minusIndex, plusIndex, list, nums);
-		if(minusIndex.size() > 0 && plusIndex.size() > 0 && zeroIndex.size() > 0) 
-			oneMinusOneZeroOnePlus(minusIndex, plusIndex, zeroIndex, list, nums);
-		if(zeroIndex.size() > 2) { 
+		
+		if(minusCount > 1 && plusCount > 0) 
+			twoMinusOnePlus(minusValue, plusValue, list, minusList, plusList);
+		if(minusCount > 0 && plusCount > 1) 
+			oneMinusTwoPlus(minusValue, plusValue, list, minusList, plusList);
+		if(minusCount > 0 && plusCount > 0 && zeroCount > 0) 
+			oneMinusOnePlus(minusValue, plusValue, list, minusList, plusList);
+		if(zeroCount > 2) { 
 			list.add(new ArrayList<Integer>());
 			list.get(list.size()-1).add(0);
 			list.get(list.size()-1).add(0);
 			list.get(list.size()-1).add(0);
 		}
+		
 		System.out.println(list.size());
 		return list;
     }
 	
-	void twoMinusOnePlus(ArrayList<Integer> minusIndex, ArrayList<Integer> plusIndex, List<List<Integer>> list, int[] nums) {
-		int count = Math.max(minusIndex.size(), plusIndex.size());
-		int minus = 0;
-		int plus = 0;
-		int mIndex = 0;
-		int mMaxIndex =  minusIndex.get(minusIndex.size()-1);
-		int pIndex = plusIndex.get(plusIndex.size()-1);
-		int pMinIndex = plusIndex.get(0);
-		while(count > 0) {
-			minus = Math.abs(nums[mIndex] + nums[mIndex+1]);
-			plus = nums[pIndex];
-			if(minus == plus) {
-				list.add(new ArrayList<Integer>());
-				list.get(list.size()-1).add(nums[mIndex]);
-				list.get(list.size()-1).add(nums[mIndex+1]);
-				list.get(list.size()-1).add(plus);
-				mIndex = (mIndex < mMaxIndex-1) ? mIndex+1:mIndex;
-				pIndex = (pIndex > pMinIndex) ? pIndex-1:pIndex;
+	void twoMinusOnePlus(HashMap<Integer, Integer> minusValue,
+						 HashMap<Integer, Integer> plusValue,
+						 List<List<Integer>> list,
+						 ArrayList<Integer> minusList,
+						 ArrayList<Integer> plusList) {
+		int iValue = 0, jValue = 0, sum = 0;
+		int maxPlusValue = plusList.get(plusList.size()-1);
+		for(int i = 0; i < minusList.size()-1; i++) {
+			iValue = minusList.get(i);
+			if(-maxPlusValue > iValue) continue;
+			for(int j = i + 1; j < minusList.size(); j++) {
+				jValue = minusList.get(j);
+				sum = -(iValue + jValue);
+				if(plusValue.containsKey(sum)) {
+					ArrayList<Integer> exam = new ArrayList<>();
+					exam.add(iValue);
+					exam.add(jValue);
+					exam.add(sum);
+					if(!list.contains(exam)) list.add(exam);
+				}
 			}
-			else if(minus > plus) {
-				mIndex = (mIndex < mMaxIndex-1) ? mIndex+1:mIndex;
-			}
-			else{
-				pIndex = (pIndex > pMinIndex) ? pIndex-1:pIndex;
-			}
-			count--;
 		}
 	}
 	
-	void oneMinusTwoPlus(ArrayList<Integer> minusIndex, ArrayList<Integer> plusIndex, List<List<Integer>> list, int[] nums) {
-		int minusCount = minusIndex.size();
-		int plusCount = plusIndex.size();
-		int count = Math.max(minusCount, plusCount);
-		int minus = 0;
-		int plus = 0;
-		int mIndex = 0;
-		int mMaxIndex =  minusIndex.get(minusIndex.size()-1);
-		int pIndex = plusIndex.get(plusIndex.size()-1);
-		int pMinIndex = plusIndex.get(0);
-		while(count > 0) {
-			minus = nums[mIndex];
-			plus = nums[pIndex] + nums[pIndex-1];
-			if(minus == plus) {
-				list.add(new ArrayList<Integer>());
-				list.get(list.size()-1).add(minus);
-				list.get(list.size()-1).add(nums[pIndex-1]);
-				list.get(list.size()-1).add(nums[pIndex]);
-				mIndex = (mIndex < mMaxIndex) ? mIndex+1:mIndex;
-				pIndex = (pIndex > pMinIndex+1) ? pIndex-1:pIndex;
+	void oneMinusTwoPlus(HashMap<Integer, Integer> minusValue,
+						 HashMap<Integer, Integer> plusValue,
+				 		 List<List<Integer>> list,
+						 ArrayList<Integer> minusList,
+						 ArrayList<Integer> plusList) {
+		int iValue = 0, jValue = 0, sum = 0;
+		int minMinusValue = minusList.get(0);
+		for(int i = plusList.size()-1; i >= 1; i--) {
+			iValue = plusList.get(i);
+			if(-minMinusValue < iValue) continue;
+			for(int j = i - 1; j >= 0; j--) {
+				jValue = plusList.get(j);
+				sum = -(iValue + jValue);
+				if(minusValue.containsKey(sum)) {
+					ArrayList<Integer> exam = new ArrayList<>();
+					exam.add(sum);
+					exam.add(iValue);
+					exam.add(jValue);
+					if(!list.contains(exam)) list.add(exam);
+				}
 			}
-			else if(minus > plus) {
-				mIndex = (mIndex < mMaxIndex) ? mIndex+1:mIndex;
-			}
-			else {
-				pIndex = (pIndex > pMinIndex+1) ? pIndex-1:pIndex;
-			}
-			count--;
 		}
-		
 	}
 	
-	void oneMinusOneZeroOnePlus(ArrayList<Integer> minusIndex, ArrayList<Integer> plusIndex, 
-								ArrayList<Integer> zeroIndex, List<List<Integer>> list, int[] nums) {
-		int minusCount = minusIndex.size();
-		int plusCount = plusIndex.size();
-		int count = Math.max(minusCount, plusCount);
-		int minus = 0;
-		int plus = 0;
-		int mIndex = 0;
-		int mMaxIndex =  minusIndex.get(minusIndex.size()-1);
-		int pIndex = plusIndex.get(plusIndex.size()-1);
-		int pMinIndex = plusIndex.get(0);
-		while(count > 0) {
-			minus = Math.abs(nums[mIndex]);
-			plus = nums[pIndex];
-			if(minus == plus) {
-				list.add(new ArrayList<Integer>());
-				list.get(list.size()-1).add(minus);
-				list.get(list.size()-1).add(0);
-				list.get(list.size()-1).add(plus);
-				mIndex = (mIndex < mMaxIndex) ? mIndex+1:mIndex;
-				pIndex = (pIndex > pMinIndex) ? pIndex-1:pIndex;
+	void oneMinusOnePlus(HashMap<Integer, Integer> minusValue,
+						 HashMap<Integer, Integer> plusValue,
+			 			 List<List<Integer>> list,
+						 ArrayList<Integer> minusList,
+						 ArrayList<Integer> plusList) {
+		int iValue = 0;
+		for(int i = 0; i < minusList.size(); i++) {
+			iValue = -(minusList.get(i));
+			if(plusValue.containsKey(iValue)) {
+				ArrayList<Integer> exam = new ArrayList<>();
+				exam.add(minusList.get(i));
+				exam.add(0);
+				exam.add(iValue);
+				if(!list.contains(exam)) list.add(exam);
 			}
-			else if(minus > plus) {
-				mIndex = (mIndex < mMaxIndex) ? mIndex+1:mIndex;
-			}
-			else {
-				pIndex = (pIndex > pMinIndex) ? pIndex-1:pIndex;
-			}
-			count--;
 		}
-		
-		
 	}
+	
 }

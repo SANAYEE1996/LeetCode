8f59907758ea263361b430a0f1630dbc9b553ef1
SANAYEE <dudtkd0219@gmail.com>
2022-07-19 23:59:47 +0900

    15 ms, faster than 40.80% // 43.1 MB, less than 83.32%

--- src/medium/LengthOfLongestSubstring.java
+++ src/medium/LengthOfLongestSubstring.java
@@ -1,25 +1,25 @@
 package medium;
 
-import java.util.ArrayList;
-
 public class LengthOfLongestSubstring {
 	 public int lengthOfLongestSubstring(String s) {
 		 if(s.equals("")) return 0;
-		 ArrayList<Character> list = new ArrayList<>();
-		 list.add(s.charAt(0));
-		 int max = list.size();
+		 StringBuilder sb = new StringBuilder();
+		 sb.append(s.charAt(0));
+		 String exam = "";
 		 int index = 0;
+		 int max = 0;
 		 for(int i = 1; i < s.length(); i++) {
-			 if(list.contains(s.charAt(i))) {
-				 max = Math.max(max, list.size());
-				 index = list.indexOf(s.charAt(i));
-				 list.remove(index);
-				 list.add(s.charAt(i));
+			 max = Math.max(max, sb.toString().length());
+			 index = sb.toString().lastIndexOf(String.valueOf(s.charAt(i)));
+			 if(index >= 0) {
+				 exam = sb.toString().substring(index+1);
+				 sb.setLength(0);
+				 sb.append(exam);
+				 sb.append(s.charAt(i));
 				 continue;
 			 }
-			 list.add(s.charAt(i));
+			 sb.append(s.charAt(i));
 		 }
-		 System.out.println(list);
-		 return Math.max(list.size(), max);
+		 return Math.max(max, sb.toString().length());
 	 }
 }

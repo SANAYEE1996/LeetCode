bdb659b06412eb24b58d05f32b170ca11f94fd28
SANAYEE <dudtkd0219@gmail.com>
2022-07-26 13:18:53 +0900

    리스트노드 파헤쳐보기

--- src/medium/AddTwoNumbers.java
+++ src/medium/AddTwoNumbers.java
@@ -3,16 +3,38 @@ package medium;
 
 
 public class AddTwoNumbers {
-	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode exam01 = l1;
+		ListNode exam02 = l2;
+		
+		
+		while(exam01 != null || exam02 != null) {
+			exam01 = (exam01 != null) ? exam01.next;
+			exam02 = exam02.next;
+		}
+		
+		
 		return l1;
     }
 	
 	public static void main(String[] args) {
-		ListNode n1 = new ListNode();
-		n1.val = 2;
-		n1.next = n1;
-		System.out.println(n1.val);
-		System.out.println(n1.next.next.val);
+		ListNode n1 = new ListNode(1);
+		ListNode n2 = new ListNode(2);
+		ListNode n3 = new ListNode(3);
+		n1.next = n2;
+		n2.next = n3;
+//		System.out.println(n1.val);
+//		System.out.println(n1.next.next.val);
+//		System.out.println(n1.next == null);
+//		System.out.println(n1.next.next == null);
+//		System.out.println(n1.next.next.next == null);
+		
+		ListNode exam = n1;
+		
+		while(exam != null) {
+			System.out.println(exam.val);
+			exam = exam.next;
+		}
+		
 	}
 }

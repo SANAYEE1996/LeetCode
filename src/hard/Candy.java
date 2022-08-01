03f18a8238cdd9e0a14dba504f09653188b9878e
SANAYEE <dudtkd0219@gmail.com>
2022-08-01 09:25:42 +0900

    사탕 자선 사업

---
+++ src/hard/Candy.java
@@ -0,0 +1,12 @@
+package hard;
+
+import java.util.Arrays;
+
+public class Candy {
+	public int candy(int[] ratings) {
+        System.out.println(Arrays.toString(ratings));
+        int answer = ratings.length;
+              
+        return answer;
+    }
+}

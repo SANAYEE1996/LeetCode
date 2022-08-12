edfb7401c4e29433813e935bb1a04301791d8af2
SANAYEE <dudtkd0219@gmail.com>
2022-08-12 09:52:49 +0900

    1 ms, faster than 99.77% // 49 MB, less than 40.22%

--- src/hard/Trap.java
+++ src/hard/Trap.java
@@ -10,22 +10,41 @@ public class Trap {
 		int count = 0;
 		int sumArea = 0;
 		int examSum = 0;
+		int maxValue = 0;
+		int maxValueIndex = 0;
 		for(int i = 0; i < height.length; i++) {
+			if(height[i] > maxValue) {
+				maxValue = height[i];
+				maxValueIndex = i;
+			}
+		}
+		
+		for(int i = 0; i < maxValueIndex; i++) {
 			examSum = 0;
-			for(int j = i+1; j < height.length; j++) {
+			for(int j = i+1; j <= maxValueIndex; j++) {
 				count = j;
 				if(height[i] <= height[j]) 
 					break;
 				examSum += (height[i]-height[j]);
-				if(j == height.length-1)
-					examSum = 0;
-				System.out.println(examSum);
+				//System.out.println(examSum + " " +(height[i]-height[j]));
 			}
-			i = count;
-			System.out.println("i value : " +i +" exam Sum : " +examSum);
+			//System.out.println("i value : " +i +" exam Sum : " +examSum);
+			i = count-1;
 			sumArea += examSum;
 		}
-		
+		for(int i = height.length-1; i > maxValueIndex; i--) {
+			examSum = 0;
+			for(int j = i-1; j >= maxValueIndex; j--) {
+				count = j;
+				if(height[i] <= height[j]) 
+					break;
+				examSum += (height[i]-height[j]);
+			}
+			//System.out.println("i value : " +i +" exam Sum : " +examSum);
+			i = count+1;
+			sumArea += examSum;
+		}
+		//System.out.println("the answer is : "+sumArea + " // time complexiblity : " +time);
 		return sumArea;
     }
 }

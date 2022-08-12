2fa97cf323479b5e9aa23c100e76901677df767a
SANAYEE <dudtkd0219@gmail.com>
2022-08-12 17:43:31 +0900

    집가서 풀 예정

--- src/hard/Candy.java
+++ src/hard/Candy.java
@@ -3,10 +3,34 @@ package hard;
 import java.util.Arrays;
 
 public class Candy {
+	
 	public int candy(int[] ratings) {
+        int answer = 0;
+        int[] candyCountArray = new int[ratings.length];
+        int lastIndex = candyCountArray.length-1;
+        for(int i = 0; i < candyCountArray.length; i++) {
+        	candyCountArray[i] = 1;
+        }
+        candyCountArray[0] = (candyCountArray[0] > candyCountArray[1]) ? 2 : 1;
+        for(int i = 1; i < ratings.length-1; i++){
+            if(ratings[i-1] < ratings[i] && ratings[i] > ratings[i+1]) {
+            	candyCountArray[i] = Math.max(candyCountArray[i-1], candyCountArray[i+1]) +1;
+            }
+            if(ratings[i-1] < ratings[i] && ratings[i] <= ratings[i+1]) {
+            	candyCountArray[i] = candyCountArray[i-1] +1;
+            	candyCountArray[i+1] = (candyCountArray[i+1] < candyCountArray[i]) ? candyCountArray[i]+1 : candyCountArray[i+1];
+            }
+            if(ratings[i-1] >= ratings[i] && ratings[i] > ratings[i+1]) {
+            	candyCountArray[i] = candyCountArray[i+1] +1;
+            	candyCountArray[i-1] = (candyCountArray[i-1] < candyCountArray[i]) ? candyCountArray[i]+1 : candyCountArray[i-1];
+            }
+        }
+        candyCountArray[lastIndex] = (candyCountArray[lastIndex] > candyCountArray[lastIndex-1]) ? candyCountArray[lastIndex-1]+1 : 1;
+        for(int i : candyCountArray) {
+        	answer += i;
+        }
         System.out.println(Arrays.toString(ratings));
-        int answer = ratings.length;
-        
+        System.out.println(Arrays.toString(candyCountArray));
         return answer;
     }
 }
--- src/hard/test/IntegerationTest.java
+++ src/hard/test/IntegerationTest.java
@@ -31,12 +31,11 @@ class IntegerationTest {
 	@Test
 	void test() {
 		//String[] longStrArray = {"si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"};
-		List<String> inputList = new ArrayList<>(Arrays.asList(new String[] {"hot","dot","dog","lot","log","cog"}));
-		List<String> inputList2 = new ArrayList<>(Arrays.asList(new String[] {"hot","dot","dog","lot","log"}));
+//		List<String> inputList = new ArrayList<>(Arrays.asList(new String[] {"hot","dot","dog","lot","log","cog"}));
+//		List<String> inputList2 = new ArrayList<>(Arrays.asList(new String[] {"hot","dot","dog","lot","log"}));
 //		List<String> inputList3 = new ArrayList<>(Arrays.asList(longStrArray));
-		System.out.println(candy.candy(new int[] {1,0,2}));
-		System.out.println(ladderLength.ladderLength("hit", "cog", inputList));
-		System.out.println(ladderLength.ladderLength("hit", "cog", inputList2));
+//		System.out.println(ladderLength.ladderLength("hit", "cog", inputList));
+//		System.out.println(ladderLength.ladderLength("hit", "cog", inputList2));
 //		System.out.println(ladderLength.ladderLength("qa", "sq", inputList3));
 	}
 	
@@ -61,6 +60,14 @@ class IntegerationTest {
 			value--;
 		}
 		assertEquals(0,trap.trap(exam));
+	}
+	
+	@Test
+	void candyTest01() {
+		System.out.println(candy.candy(new int[] {1,0,2}));
+		System.out.println(candy.candy(new int[] {1,2,2}));
+		System.out.println(candy.candy(new int[] {1,2,87,87,87,2,1}));
+		System.out.println(candy.candy(new int[] {1,3,4,5,2}));
 	}
 
 }

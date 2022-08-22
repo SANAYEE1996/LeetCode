b325643ac52893c777399cbd6f554edf8ee2ad94
SANAYEE <dudtkd0219@gmail.com>
2022-08-22 13:18:59 +0900

    다음 순열

---
+++ src/medium/NextPermutation.java
@@ -0,0 +1,11 @@
+package medium;
+
+import java.util.Arrays;
+
+public class NextPermutation {
+	public void nextPermutation(int[] nums) {
+        
+		
+		System.out.println(Arrays.toString(nums));
+    }
+}	

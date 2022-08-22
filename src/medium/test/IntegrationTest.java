c0765c76506fa40db570f4154a3a36dd0fcaecbc
SANAYEE <dudtkd0219@gmail.com>
2022-08-22 13:51:15 +0900

    통합 테스트

--- src/medium/test/IntegrationTest.java
+++ src/medium/test/IntegrationTest.java
@@ -17,6 +17,12 @@ class IntegrationTest {
 	@Test
 	void test() {
 		np.nextPermutation(new int[] {1,2,3});
+		np.nextPermutation(new int[] {3,9,8,7,5});
+		np.nextPermutation(new int[] {3,9,7,8,5});
+		np.nextPermutation(new int[] {1,1,5});
+		np.nextPermutation(new int[] {1});
+		np.nextPermutation(new int[] {1,1});
+		np.nextPermutation(new int[] {5,4,3,2,1});
 	}
 
 }

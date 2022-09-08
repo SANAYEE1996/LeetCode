a2ce42c1b36103db69b37b9571744d4e9380fe0b
SANAYEE <dudtkd0219@gmail.com>
2022-09-08 16:24:53 +0900

    4 ms, faster than 46.78% // 67.4 MB, less than 71.49%

--- src/medium/JumpGame.java
+++ src/medium/JumpGame.java
@@ -3,15 +3,29 @@ package medium;
 public class JumpGame {
 	public boolean canJump(int[] nums) {
         
+		int nowReach = 0;
+		int maxReach = 0;
 		
+		for(int i = 0; i < nums.length; i++) {
+			nowReach = nums[i];
+			maxReach = Math.max(nowReach, maxReach);
+			if(maxReach == 0 && i < nums.length-1) {
+				return false;
+			}
+			maxReach--;
+		}
 		
-		
-		return false;
+		return true;
     }
 	
 	
 	
 	public int jump(int[] nums) {
+		
+		
+		
+		
+		
 		return 0;
     }
 }

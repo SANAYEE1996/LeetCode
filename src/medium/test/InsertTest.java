afba3362c0ef0856b8dee142923491cc295d1ae5
SANAYEE <dudtkd0219@gmail.com>
2022-09-18 00:17:27 +0900

    케이스 참.. 다시 알고리즘 생각해야함

--- src/medium/test/InsertTest.java
+++ src/medium/test/InsertTest.java
@@ -22,7 +22,16 @@ class InsertTest {
 		 * */
 		int[][] a = {{1,2},{3,5},{6,7},{8,10},{12,16}};
 		int[] b = {4,8};
+		int[][] c = {{1,3},{6,9}};
+		int[] d = {2,5};
+		int[][] e = {};
+		int[] f = {5,7};
+		int[][] g = {{1,5}};
+		int[] h = {2,3};
 		System.out.println(insert.insert(a, b));
+		System.out.println(insert.insert(c, d));
+		System.out.println(insert.insert(e, f));
+		System.out.println(insert.insert(g, h));
 	}
 
 }

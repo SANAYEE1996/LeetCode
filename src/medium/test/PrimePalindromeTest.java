10ecffd827a4cb1122ae4fef91b3929ea634f56f
SANAYEE <dudtkd0219@gmail.com>
2022-09-29 02:36:45 +0900

    하 이거 시간복잡도 개극혐

--- src/medium/test/PrimePalindromeTest.java
+++ src/medium/test/PrimePalindromeTest.java
@@ -21,7 +21,7 @@ class PrimePalindromeTest {
 		assertEquals(5, pp.primePalindrome(5));
 		assertEquals(11, pp.primePalindrome(8));
 		assertEquals(101, pp.primePalindrome(17));
-		//System.out.println(pp.primePalindrome(9989900));
+		System.out.println(pp.primePalindrome(9989900));
 		assertEquals(true, pp.isPalindrome(5));
 		assertEquals(true, pp.isPalindrome(121));
 		assertEquals(true, pp.isPalindrome(12455421));
@@ -30,6 +30,18 @@ class PrimePalindromeTest {
 		assertEquals(false, pp.isPalindrome(12455431));
 		assertEquals(false, pp.isPalindrome(1245431));
 		assertEquals(true, pp.isPalindrome(1245421));
+		assertEquals(false, pp.isPalindrome(1245420));
+//		System.out.println(pp.isPrime(101));
+//		assertEquals(1, pp.getIntegerLength(1));
+//		assertEquals(1, pp.getIntegerLength(4));
+//		assertEquals(1, pp.getIntegerLength(5));
+//		assertEquals(1, pp.getIntegerLength(9));
+//		assertEquals(2, pp.getIntegerLength(10));
+//		assertEquals(2, pp.getIntegerLength(47));
+//		assertEquals(3, pp.getIntegerLength(100));
+//		assertEquals(3, pp.getIntegerLength(357));
+//		assertEquals(4, pp.getIntegerLength(1000));
+//		assertEquals(10, pp.getIntegerLength(2104080000));
 	}
 
 }

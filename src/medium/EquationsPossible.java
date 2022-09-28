5bbf88e2b3b27e15401090b0e8799f7e234157a9
SANAYEE <dudtkd0219@gmail.com>
2022-09-28 12:05:10 +0900

    아직 못품

--- src/medium/EquationsPossible.java
+++ src/medium/EquationsPossible.java
@@ -49,4 +49,8 @@ public class EquationsPossible {
 			map.put(tailKey, new ArrayList<Character>());
 		}
 	}
+	
+	public static void main(String[] args) {
+		System.out.println(new StringBuilder("1234").insert(2, " : ").toString());
+	}
 }

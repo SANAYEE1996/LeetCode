c64afa4fd3630025731cf50ccb986c157318ecae
SANAYEE <dudtkd0219@gmail.com>
2022-10-01 00:42:45 +0900

    test case

--- src/medium/test/FindClosestElementsTest.java
+++ src/medium/test/FindClosestElementsTest.java
@@ -23,8 +23,11 @@ class FindClosestElementsTest {
 	@Test
 	void test() {
 		int[] a = {1,2,3,4,5};
-		assertEquals(new ArrayList<Integer>(Arrays.asList(new Integer[] {})), s.findClosestElements(a, 4, 3));
-		
+		assertEquals(new ArrayList<Integer>(Arrays.asList(new Integer[] {1,2,3,4})), s.findClosestElements(a, 4, 3));
+		assertEquals(new ArrayList<Integer>(Arrays.asList(new Integer[] {1,2,3,4})), s.findClosestElements(a, 4, -1));
+		System.out.println(s.findClosestElements(new int[] {1,1,1,10,10,10},1,9));
+		System.out.println(s.findClosestElements(new int[] {1},1,-100));
+		System.out.println(s.findClosestElements(new int[] {1,2,3,4,5},4,3));
 	}
 
 }

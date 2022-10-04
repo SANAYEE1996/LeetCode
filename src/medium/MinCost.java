9c96b155e695241825c29ef5ac59c05f861a58bd
SANAYEE <dudtkd0219@gmail.com>
2022-10-04 13:11:49 +0900

    뭐지

--- src/medium/MinCost.java
+++ src/medium/MinCost.java
@@ -1,12 +1,37 @@
 package medium;
 
+import java.util.Arrays;
+
 public class MinCost {
 	public int minCost(String colors, int[] neededTime) {
-        
-        for(int i = 0; i < colors.length(); i++){
-            
+//        System.out.println("input : " +colors);
+//        System.out.println("time : " +Arrays.toString(neededTime));
+		int answer = 0;
+		int startIndex = 0;
+		int lastIndex = 0;
+        for(int i = 0; i < colors.length()-1; i++){
+        	startIndex = i;
+            while(i < colors.length()-1 && colors.charAt(i) == colors.charAt(i+1)) {
+            	i++;
+            }
+            lastIndex = i;
+            if(startIndex != lastIndex) {
+            	answer += getAnswer(neededTime, startIndex, lastIndex);
+            }
         }
         
-        return 0;
+        return answer;
     }
+	
+	int getAnswer(int[] neededTime, int startIndex, int lastIndex) {
+		int aSum = 0;
+		int bSum = 0;
+		for(int i = startIndex; i <= lastIndex; i += 2) {
+			aSum += neededTime[i];
+		}
+		for(int i = startIndex+1; i <= lastIndex; i+=2){
+			bSum += neededTime[i];
+		}
+		return Math.min(aSum, bSum);
+	}
 }
--- src/medium/test/MinCostTest.java
+++ src/medium/test/MinCostTest.java
@@ -18,7 +18,13 @@ class MinCostTest {
 
 	@Test
 	void test() {
-		assertEquals(0, mincost.minCost("", new int[] {}));
+//		assertEquals(2, mincost.minCost("aabcc", new int[] {1,2,3,4,1}));
+//		assertEquals(3, mincost.minCost("abaac", new int[] {1,2,3,4,5}));
+//		assertEquals(26, mincost.minCost("aaabbbabbbb", new int[] {3,5,10,7,5,3,5,5,4,8,1}));
+		System.out.println(mincost.minCost("aabcc", new int[] {1,2,3,4,1}));
+		System.out.println(mincost.minCost("abaac", new int[] {1,2,3,4,5}));
+		System.out.println(mincost.minCost("acaaa", new int[] {1,2,3,4,5}));
+		System.out.println(mincost.minCost("aaabbbabbbb", new int[] {3,5,10,7,5,3,5,5,4,8,1}));
 	}
 
 }

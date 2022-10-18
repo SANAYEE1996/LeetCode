c880c103a5298f5992a5b8fea32c0646c3e78c0e
SANAYEE <dudtkd0219@gmail.com>
2022-10-18 20:53:49 +0900

    4 ms, faster than 5.34% // 44.6 MB, less than 6.93%

--- src/medium/Search.java
+++ src/medium/Search.java
@@ -1,5 +1,7 @@
 package medium;
 
+import java.util.Arrays;
+
 public class Search {
 	public int search(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++){
@@ -8,5 +10,29 @@ public class Search {
             }
         }
         return -1;
+    }
+	
+	public boolean search81(int[] nums, int target) {
+        Arrays.sort(nums);
+        return isHere(nums, target);
+    }
+    
+    private boolean isHere(int[] nums, int target){
+        int start = 0;
+        int end = nums.length-1;
+        int mid;
+        while(start <= end){
+            mid = (end + start)/2;
+            if(nums[mid] == target){
+                return true;
+            }
+            else if(nums[mid] > target){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return false;
     }
 }

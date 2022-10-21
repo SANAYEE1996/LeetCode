412298e4c562f6b9707234205ff986b94d590f96
SANAYEE <dudtkd0219@gmail.com>
2022-10-21 19:37:16 +0900

    default setting

---
+++ src/easy/ContainsNearbyDuplicate.java
@@ -0,0 +1,7 @@
+package easy;
+
+public class ContainsNearbyDuplicate {
+	public boolean containsNearbyDuplicate(int[] nums, int k) {
+        return false;
+    }
+}	
---
+++ src/easy/test/ContainsNearbyDuplicateTest.java
@@ -0,0 +1,24 @@
+package easy.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import easy.ContainsNearbyDuplicate;
+
+class ContainsNearbyDuplicateTest {
+
+	private ContainsNearbyDuplicate c;
+	
+	@BeforeEach
+	public void setup() {
+		c = new ContainsNearbyDuplicate();
+	}
+	
+	@Test
+	void test() {
+		assertEquals(1,1);
+	}
+
+}

e10a99aa332a0110f3a8d910c56e8d1ceded46e3
SANAYEE <dudtkd0219@gmail.com>
2022-10-23 20:38:36 +0900

    시간복잡도를 개선하늦중

--- src/easy/FindErrorNums.java
+++ src/easy/FindErrorNums.java
@@ -34,10 +34,33 @@ public class FindErrorNums {
         return new int[]{0,0};
     }
 	
+	public int[] findErrorNumsTimeImproved(int[] nums) {
+        Arrays.sort(nums);
+        int startIndex = 0;
+        int notExistedNumber = 0;
+        for(int i = 0; i < nums.length; i++) {
+        	if(nums[i] != i+1) {
+        		notExistedNumber = i+1;
+        		startIndex = (i > 0) ? i : 1;
+        		break;
+        	}
+        }
+        for(int i = startIndex; i < nums.length; i++) {
+        	if(nums[i] == nums[i-1]) {
+        		return new int[] {nums[i], notExistedNumber};
+        	}
+        }
+        return new int[]{0,0};
+    }
+	
 	public static void main(String[] args) {
 		FindErrorNums s = new FindErrorNums();
 		System.out.println(Arrays.toString(s.findErrorNums(new int[] {1,2,2,4})));
 		System.out.println(Arrays.toString(s.findErrorNums(new int[] {1,1})));
 		System.out.println(Arrays.toString(s.findErrorNums(new int[] {3,2,3,4,6,5})));
+		
+		System.out.println(Arrays.toString(s.findErrorNumsTimeImproved(new int[] {1,2,2,4})));
+		System.out.println(Arrays.toString(s.findErrorNumsTimeImproved(new int[] {1,1})));
+		System.out.println(Arrays.toString(s.findErrorNumsTimeImproved(new int[] {3,2,3,4,6,5})));
 	}
 }

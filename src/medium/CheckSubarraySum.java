023c5d126273cd83dbf65d22ef41fc686d14f067
SANAYEE <dudtkd0219@gmail.com>
2022-10-26 21:04:22 +0900

    아

--- src/medium/CheckSubarraySum.java
+++ src/medium/CheckSubarraySum.java
@@ -1,11 +1,30 @@
 package medium;
 
+import java.util.Arrays;
+
 public class CheckSubarraySum {
 	public boolean checkSubarraySum(int[] nums, int k) {
+		if(nums.length < 2) return false;
         for(int i = 0; i < nums.length; i++) {
         	nums[i] = nums[i]%k;
         }
-		
+		System.out.println(Arrays.toString(nums));
+		int sum = nums[0];
+		int startIndex = 0;
+		for(int i = 1; i < nums.length; i++) {
+			sum += nums[i];
+			System.out.println("startIndex : "+startIndex+" // nowIndex : "+i+" // sum : "+sum);
+			if(sum == k || sum == 0) {
+				return true;
+			}
+			else if(sum > k) {
+				sum -= nums[startIndex];
+				if((sum == k || sum == 0) && i - startIndex > 1) {
+					return true;
+				}
+				startIndex++;
+			}
+		}
 		return false;
     }
 }
--- src/medium/test/CheckSubarraySumTest.java
+++ src/medium/test/CheckSubarraySumTest.java
@@ -17,28 +17,20 @@ class CheckSubarraySumTest {
 		c = new CheckSubarraySum();
 	}
 	
-//	@Test
-//	void testCaseTrue() {
-//		assertTrue(c.checkSubarraySum(new int[] {}, 3));
-//		assertTrue(c.checkSubarraySum(new int[] {}, 3));
-//		assertTrue(c.checkSubarraySum(new int[] {}, 3));
-//		assertTrue(c.checkSubarraySum(new int[] {}, 3));
-//		assertTrue(c.checkSubarraySum(new int[] {}, 3));
-//		assertTrue(c.checkSubarraySum(new int[] {}, 3));
-//		assertTrue(c.checkSubarraySum(new int[] {}, 3));
-//	}
+	@Test
+	void testCaseTrue() {
+		assertTrue(c.checkSubarraySum(new int[] {23,2,4,6,7}, 6));
+		assertTrue(c.checkSubarraySum(new int[] {23,2,6,4,7}, 6));
+		assertTrue(c.checkSubarraySum(new int[] {6,12,18,24,30}, 6));
+		assertTrue(c.checkSubarraySum(new int[] {2,4,6}, 6));
+		assertTrue(c.checkSubarraySum(new int[] {23,2,4,6,6}, 7));
+	}
 	
 	@Test
 	void testCaseFalse() {
-		assertFalse(c.checkSubarraySum(new int[] {}, 3));
-		assertFalse(c.checkSubarraySum(new int[] {}, 3));
-		assertFalse(c.checkSubarraySum(new int[] {}, 3));
-		assertFalse(c.checkSubarraySum(new int[] {}, 3));
-		assertFalse(c.checkSubarraySum(new int[] {}, 3));
-		assertFalse(c.checkSubarraySum(new int[] {}, 3));
-		assertFalse(c.checkSubarraySum(new int[] {}, 3));
-		assertFalse(c.checkSubarraySum(new int[] {}, 3));
-		assertFalse(c.checkSubarraySum(new int[] {}, 3));
+		assertFalse(c.checkSubarraySum(new int[] {23,2,6,4,7}, 13));
+		assertFalse(c.checkSubarraySum(new int[] {1,2,39,16,7}, 17));
+		assertFalse(c.checkSubarraySum(new int[] {46,46,46,43,2}, 47));
 	}
 
 }

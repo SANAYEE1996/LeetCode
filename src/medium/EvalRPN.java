fc5d6516598b6ab40fef34c37a62e59948e1e5fe
SANAYEE1996 <dudtkd0219@gmail.com>
2022-11-02 16:54:25 +0900

    6 ms, faster than 91.85% // 44.3 MB, less than 34.43%

--- src/medium/EvalRPN.java
+++ src/medium/EvalRPN.java
@@ -1,9 +1,53 @@
 package medium;
 
+import java.util.ArrayList;
+
 public class EvalRPN {
 	public int evalRPN(String[] tokens) {
         
-		
-		return 0;
+		ArrayList<Integer> list = new ArrayList<>();
+		for(String str : tokens){
+			if(isCalculate(str)){
+				goCalculate(list, str);
+			}
+			else{
+				list.add(Integer.parseInt(str));
+			}
+		}
+		return list.get(0);
     }
+	
+	private void goCalculate(ArrayList<Integer> list, String a){
+		int before = list.get(list.size() - 2);
+		int after = list.get(list.size() - 1);
+		list.remove(list.size()-1);
+		list.remove(list.size()-1);
+		if(a.equals("+")){
+			list.add(before + after);
+		}
+		else if(a.equals("-")){
+			list.add(before - after);
+		}
+		else if(a.equals("/")){
+			list.add(before / after);
+		}
+		else if(a.equals("*")){
+			list.add(before * after);
+		}
+	}
+
+	private boolean isCalculate(String a){
+		if(a.equals("+") || a.equals("-") || a.equals("/") || a.equals("*")){
+			return true;
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		EvalRPN s = new EvalRPN();
+		
+		System.out.println(s.evalRPN(new String[] {"10","6","9","3","+","-11","*","/","*","17","+","5","+"}));
+		System.out.println(s.evalRPN(new String[] {"2","1","+","3","*"}));
+		System.out.println(s.evalRPN(new String[] {"4","13","5","/","+"}));
+	}
 }

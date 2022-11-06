5f4d4db5d8da4d8726fe08eed9a419cd98e622b5
SANAYEE <dudtkd0219@gmail.com>
2022-11-06 20:57:09 +0900

    알것 같으면서..

--- src/medium/MaximumSubarraySum.java
+++ src/medium/MaximumSubarraySum.java
@@ -8,33 +8,45 @@ import java.util.LinkedHashSet;
 
 public class MaximumSubarraySum {
 	public long maximumSubarraySum(int[] nums, int k) {
-		System.out.println("input : " +Arrays.toString(nums));
+//		System.out.println("input : " +Arrays.toString(nums));
         LinkedHashSet<Integer> numSet = new LinkedHashSet<>();
 		long nowSum = 0;
 		ArrayList<Long> sumList = new ArrayList<>();
-		int firstValue = nums[0];
 		for(int i = 0; i < k; i++) {
-			nowSum += nums[i];
 			numSet.add(nums[i]);
+		}
+		Iterator<Integer> iter = numSet.iterator();
+		while(iter.hasNext()) {
+			nowSum += iter.next();
 		}
 		if(numSet.size() == k) {
 			sumList.add(nowSum);
 		}
+		int beforeSetSize = numSet.size();
 		for(int i = k; i < nums.length; i++) {
-			nowSum -= numSet.iterator().next();
 			nowSum += nums[i];
-			numSet.remove(firstValue);
 			numSet.add(nums[i]);
-			firstValue = numSet.iterator().next();
-			if(numSet.size() == k) {
-				sumList.add(nowSum);
+			if(beforeSetSize != numSet.size()) {
+				if(numSet.size() == k+1) {
+					nowSum -= numSet.iterator().next();
+					numSet.remove(numSet.iterator().next());
+					sumList.add(nowSum);
+				}
+				else if(numSet.size() == k) {
+					sumList.add(nowSum);
+				}
 			}
+			else {
+				nowSum -= nums[i];
+				numSet.remove(nums[i]);
+				numSet.add(nums[i]);
+				if(numSet.size() == k) {
+					sumList.add(nowSum);
+				}
+			}
+			beforeSetSize = numSet.size();
 		}
-		Iterator<Integer> iter = numSet.iterator();
-		while(iter.hasNext()) {
-			System.out.println("iter :    " +iter.next());
-		}
-		System.out.println("max value list : "+sumList);
+//		System.out.println("max value list : "+sumList);
 		if(sumList.isEmpty()) {
 			return 0;
 		}
--- src/medium/test/MaximumSubarraySumTest.java
+++ src/medium/test/MaximumSubarraySumTest.java
@@ -21,6 +21,8 @@ class MaximumSubarraySumTest {
 		assertEquals(15,mss.maximumSubarraySum(new int[] {1,5,4,2,9,9,9},3));
 		assertEquals(0,mss.maximumSubarraySum(new int[] {4,4,4},3));
 		assertEquals(24,mss.maximumSubarraySum(new int[] {1,1,1,7,8,9},3));
+		assertEquals(8,mss.maximumSubarraySum(new int[] {1,2,1,2,5,1,2},3));
+		assertEquals(8,mss.maximumSubarraySum(new int[] {3,5,3,4},2));
 	}
 
 }

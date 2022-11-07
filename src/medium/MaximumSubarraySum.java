99dbe6f17caba882e7357fc721964eac091583bc
SANAYEE1996 <dudtkd0219@gmail.com>
2022-11-07 09:49:42 +0900

    300 ms faster than 16.67% // 150.2 MB faster than 16.67%

--- src/medium/MaximumSubarraySum.java
+++ src/medium/MaximumSubarraySum.java
@@ -8,22 +8,17 @@ import java.util.LinkedHashSet;
 
 public class MaximumSubarraySum {
 	public long maximumSubarraySum(int[] nums, int k) {
-//		System.out.println("input : " +Arrays.toString(nums));
+		System.out.println("input : " +Arrays.toString(nums));
         LinkedHashSet<Integer> numSet = new LinkedHashSet<>();
-		long nowSum = 0;
 		ArrayList<Long> sumList = new ArrayList<>();
-		for(int i = 0; i < k; i++) {
-			numSet.add(nums[i]);
-		}
-		Iterator<Integer> iter = numSet.iterator();
-		while(iter.hasNext()) {
-			nowSum += iter.next();
-		}
+		long[] partSumArray = new long[1];
+		int startIndex = getStartIndex(nums, numSet, k, partSumArray);
+		long nowSum = partSumArray[0];
 		if(numSet.size() == k) {
 			sumList.add(nowSum);
 		}
 		int beforeSetSize = numSet.size();
-		for(int i = k; i < nums.length; i++) {
+		for(int i = startIndex; i < nums.length; i++) {
 			nowSum += nums[i];
 			numSet.add(nums[i]);
 			if(beforeSetSize != numSet.size()) {
@@ -37,19 +32,52 @@ public class MaximumSubarraySum {
 				}
 			}
 			else {
-				nowSum -= nums[i];
-				numSet.remove(nums[i]);
+				removePartNumset(numSet, nums[i], partSumArray);
+				partSumArray[0] += nums[i];
+				nowSum = partSumArray[0];
 				numSet.add(nums[i]);
-				if(numSet.size() == k) {
-					sumList.add(nowSum);
-				}
 			}
 			beforeSetSize = numSet.size();
 		}
-//		System.out.println("max value list : "+sumList);
+		System.out.println("max value list : "+sumList);
 		if(sumList.isEmpty()) {
 			return 0;
 		}
 		return Collections.max(sumList);
     }
+	
+	private int getStartIndex(int[] nums, LinkedHashSet<Integer> numSet, int k, long[] partSumArray) {
+		int beforeSetSize = numSet.size();
+		for(int i = 0; i < k; i++) {
+			numSet.add(nums[i]);
+			partSumArray[0] += nums[i];
+			if(numSet.size() == beforeSetSize) {
+				removePartNumset(numSet, nums[i], partSumArray);
+				partSumArray[0] += nums[i];
+				numSet.add(nums[i]);
+				return i+1;
+			}
+			beforeSetSize = numSet.size();
+		}
+		return k;
+	}
+	
+	private void removePartNumset(LinkedHashSet<Integer> numSet, int value, long[] partSumArray) {
+		Iterator<Integer> iter = numSet.iterator();
+		ArrayList<Integer> removeList = new ArrayList<>();
+		int target;
+		while(iter.hasNext()) {
+			target = iter.next();
+			removeList.add(target);
+			if(target == value) {
+				break;
+			}
+		}
+		numSet.removeAll(removeList);
+		iter = numSet.iterator();
+		partSumArray[0] = 0;
+		while(iter.hasNext()) {
+			partSumArray[0] += iter.next();
+		}
+	}
 }
--- src/medium/test/MaximumSubarraySumTest.java
+++ src/medium/test/MaximumSubarraySumTest.java
@@ -23,6 +23,8 @@ class MaximumSubarraySumTest {
 		assertEquals(24,mss.maximumSubarraySum(new int[] {1,1,1,7,8,9},3));
 		assertEquals(8,mss.maximumSubarraySum(new int[] {1,2,1,2,5,1,2},3));
 		assertEquals(8,mss.maximumSubarraySum(new int[] {3,5,3,4},2));
+		assertEquals(0,mss.maximumSubarraySum(new int[] {5,3,3,1,1},3));
+		assertEquals(8,mss.maximumSubarraySum(new int[] {3,5,2,4},2));
 	}
 
 }

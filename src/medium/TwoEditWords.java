f5995353298628896d4c6c5a19787e5f22bc7bdc
SANAYEE1996 <dudtkd0219@gmail.com>
2022-11-07 15:24:35 +0900

    default setting

---
+++ src/medium/TwoEditWords.java
@@ -0,0 +1,12 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwoEditWords {
+	public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> answerList = new ArrayList<>();
+
+        return answerList;
+    }
+}

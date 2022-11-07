e56bc806081d69a27ca3e3c8fbcd20119eca1152
SANAYEE1996 <dudtkd0219@gmail.com>
2022-11-07 16:20:32 +0900

    .

--- src/medium/SubarrayGCD.java
+++ src/medium/SubarrayGCD.java
@@ -1,9 +1,25 @@
 package medium;
 
+import java.util.ArrayList;
+
 public class SubarrayGCD {
 	public int subarrayGCD(int[] nums, int k) {
-        
+        for(int i = 0; i < nums.length; i++) {
+        	if(nums[i] % k == 0) {
+        		
+        	}
+        }
 		
 		return 0;
     }
+	
+	private int getCaseCount(ArrayList<Integer> list) {
+		int count = 0;
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i) == 1) {
+				count += list.size();
+			}
+		}
+		return count;
+	}
 }

332f52596e755d4ad572045c4584ff6f1185242e
SANAYEE <dudtkd0219@gmail.com>
2022-11-13 09:51:51 +0900

    18 ms faster than 46.28 % // 43.9 MB less than 36.8 %

--- src/medium/ReverseWords.java
+++ src/medium/ReverseWords.java
@@ -1,35 +1,26 @@
 package medium;
 
+import java.util.Arrays;
+
 public class ReverseWords {
 	public String reverseWords(String s) {
-		s = s.trim();
+		String[] strArray = s.split("\\s+");
+		System.out.println(Arrays.toString(strArray));
 		StringBuilder sb = new StringBuilder();
-		StringBuilder partSb = new StringBuilder();
-		char beforeChar = 0;
-		for(int i = s.length()-1; i >= 0; i--) {
-			if(s.charAt(i) == ' ' && beforeChar == ' ') {
-				beforeChar = ' ';
-				continue;
-			}
-			else if(s.charAt(i) == ' ' && beforeChar != ' ') {
-				beforeChar = ' ';
-				sb.append(partSb.toString());
+		for(int i = strArray.length-1; i >= 0; i--) {
+			sb.append(strArray[i]);
+			if(i != 0) {
 				sb.append(' ');
-				partSb.setLength(0);
-				continue;
 			}
-			partSb.insert(0, s.charAt(i));
-			beforeChar = s.charAt(i);
 		}
-		sb.append(partSb.toString());
 		return sb.toString();
     }
 	
 	public static void main(String[] args) {
 		ReverseWords s = new ReverseWords();
 		
-		System.out.println(s.reverseWords("the sky is blue"));
-		System.out.println(s.reverseWords("  hello world  "));
-		System.out.println(s.reverseWords("a good   example"));
+		System.out.println(s.reverseWords("the sky is blue")+ "||");
+		System.out.println(s.reverseWords("  hello world  ") + "||");
+		System.out.println(s.reverseWords("a good   example")+ "||");
 	}
 }

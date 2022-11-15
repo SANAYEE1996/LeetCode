4c68ab5e7c24a1e6394ccfb8f6d1ee7c631ef396
SANAYEE1996 <dudtkd0219@gmail.com>
2022-11-15 16:50:59 +0900

    .

--- src/medium/TreeNodeSimulator.java
+++ src/medium/TreeNodeSimulator.java
@@ -3,24 +3,16 @@ package medium;
 public class TreeNodeSimulator {
 
 	public static void main(String[] args){
-		TreeNode tree = new TreeNode(1);
-		try {
-			System.out.println(tree.val);
-			System.out.println(tree.left.val);
-		}
-		catch(NullPointerException e) {
-			e.printStackTrace();
-		}
-		try {
-			tree.left = new TreeNode(2);
-			tree.right = new TreeNode(3);
-			System.out.println(tree.left.val);
-			System.out.println(tree.right.val);
-		}
-		catch(NullPointerException e) {
-			e.printStackTrace();
-		}
-
+		TreeNode tree = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+		tree.left = new TreeNode(tree.left.val, new TreeNode(4),new TreeNode(5));
+		tree.right.left = new TreeNode(6);
+		System.out.println(tree.val);
+		System.out.println(tree.left.val);
+		System.out.println(tree.left.left.val);
+		System.out.println(tree.left.right.val);
+		System.out.println(tree.right.val);
+		System.out.println(tree.right.left.val);
+		System.out.println(tree.right.right == null);
 	}
 
 }

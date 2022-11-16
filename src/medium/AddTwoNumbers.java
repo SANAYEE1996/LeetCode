d33f5a7befcbda73917ec7ef1cb0dc1064c32cf8
SANAYEE1996 <dudtkd0219@gmail.com>
2022-11-16 14:44:44 +0900

    3 ms faster than 81.2%

--- src/medium/AddTwoNumbers.java
+++ src/medium/AddTwoNumbers.java
@@ -2,33 +2,26 @@ package medium;
 
 public class AddTwoNumbers {
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		ListNode answer = new ListNode();
-		int pivot = 0;
-		int sum = 0;
-		try {
-			while(true) {
-				sum = l1.val+l2.val+pivot;
-				if(sum >= 10) {
-					sum -= 10;
-					pivot = 1;
-				}
-				else {
-					pivot = 0;
-				}
-				answer.val = sum;
-				if(l1.next == null) {
-					break;
-				}
-				l1 = l1.next;
-				l2 = l2.next;
-				answer.next = new ListNode();
-				answer = answer.next;
-			}
-			return answer;
-		}
-		catch(NullPointerException e) {
-			return new ListNode();
-		}
+		ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+        while(l1 != null || l2 != null || carry == 1){
+            int sum = 0;
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+            carry = sum/10;
+            ListNode node = new ListNode(sum % 10);
+            curr.next = node;
+            curr = curr.next;
+        }
+        return dummy.next;
     }
 	
 	void printListNode(ListNode l1) {

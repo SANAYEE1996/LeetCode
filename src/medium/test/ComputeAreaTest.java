0f94fa43fbddc8fc9d2a4639da38c8505450a931
SANAYEE1996 <dudtkd0219@gmail.com>
2022-11-17 09:40:08 +0900

    default setting

---
+++ src/medium/test/ComputeAreaTest.java
@@ -0,0 +1,24 @@
+package medium.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import medium.ComputeArea;
+
+class ComputeAreaTest {
+	
+	private ComputeArea c;
+	
+	@BeforeEach
+	public void setup() {
+		c = new ComputeArea();
+	}
+	
+	@Test
+	void test() {
+		assertEquals(0,c.computeArea(-3,0,3,4,0,-1,9,2));
+	}
+
+}

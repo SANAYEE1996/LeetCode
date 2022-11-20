8c4d264b47527396e7acf766d566a3ac681966ee
SANAYEE <dudtkd0219@gmail.com>
2022-11-20 11:59:32 +0900

    .

--- src/hard/BasicCalculator.java
+++ src/hard/BasicCalculator.java
@@ -1,16 +1,41 @@
 package hard;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class BasicCalculator {
 	
 	public int calculate(String s) {
-		s = s.replaceAll(" ", "");
-		String[] strCalArray = s.split("\\)|\\(");
 		System.out.println("input : " +s);
-		System.out.println(Arrays.toString(strCalArray));
+		int[] countAddFlag = new int[1];
+		char beforePivot = '+';
+		char beforeCalSign = '+';
+		int calValue = 0;
+		int value = 0;
+		for(int i = 0; i < s.length(); i++) {
+			if(48 <= s.charAt(i) && s.charAt(i) <= 57) {
+				calValue = getNumberFromString(s, i, countAddFlag);
+				value = (beforeCalSign == '+') ? value + calValue : value - calValue;
+			}
+			else if(s.charAt(i) == '(') {
+				
+			}
+			else if(s.charAt(i) == ')') {
+				
+			}
+			else if(s.charAt(i) == '+') {
+				
+			}
+		}
+		
 		return 0;
     }
+	
+	private int getNumberFromString(String s, int startIndex, int[] countAddFlag) {
+		for(int i = startIndex; i < s.length(); i++) {
+			if(!(48 <= s.charAt(i) && i <= 57)) {
+				return Integer.parseInt(String.valueOf(s.substring(startIndex, i)));
+			}
+			countAddFlag[0]++;
+		}
+		return Integer.parseInt(String.valueOf(s.substring(startIndex)));
+	}
 	
 }

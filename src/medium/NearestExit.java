486ccae087b1616d01756f7d818d7762d49e9170
SANAYEE1996 <dudtkd0219@gmail.com>
2022-11-21 14:20:32 +0900

    19 ms faster than 46.95% // 68.5 MB less than 61.96%

--- src/medium/NearestExit.java
+++ src/medium/NearestExit.java
@@ -7,66 +7,54 @@ import java.util.Queue;
 public class NearestExit {
 	
 	public int nearestExit(char[][] maze, int[] entrance) {
-		printCharMap(maze);
         boolean[][] visited = new boolean[maze.length][maze[0].length];
         Queue<int[]> que = new LinkedList<>();
         
         visited[entrance[0]][entrance[1]] = true;
         que.offer(new int[] {entrance[0], entrance[1], 0});
-        int i = 0, j = 0, length = 0;
+        int i = entrance[0], j = entrance[1], length = 0;
+        if((i == 0 || i == maze.length-1) && ((0 < j && maze[i][j-1] == '.') || (j < maze[0].length-1 && maze[i][j+1] == '.'))) {
+        	return 1;
+        }
+        if((j == 0 || j == maze[0].length-1) && ((0 < i && maze[i-1][j] == '.') || (i < maze.length-1 && maze[i+1][j] == '.'))) {
+        	return 1;
+        }
         
         while(!que.isEmpty()) {
         	int[] exam = que.poll();
         	i = exam[0];
         	j = exam[1];
         	length = exam[2];
-        	System.out.println("length : " +length);
         	if(i < maze.length-1 && !visited[i+1][j] && maze[i+1][j] != '+') {
         		if(i + 1 == maze.length-1) {
-        			length++;
-        			break;
+        			return ++length;
         		}
         		visited[i+1][j] = true;
         		que.offer(new int[] {i+1, j, length+1});
         	}
         	if(i > 0 && !visited[i-1][j] && maze[i-1][j] != '+') {
         		if(i - 1 == 0) {
-        			length++;
-        			break;
+        			return ++length;
         		}
         		visited[i-1][j] = true;
         		que.offer(new int[] {i-1, j, length+1});
         	}
         	if(j < maze[0].length-1 && !visited[i][j+1] && maze[i][j+1] != '+') {
         		if(j + 1 == maze[0].length-1) {
-        			length++;
-        			System.out.println("masaka");
-        			break;
+        			return ++length;
         		}
         		visited[i][j+1] = true;
         		que.offer(new int[] {i, j+1, length+1});
         	}
         	if(j > 0 && !visited[i][j-1] && maze[i][j-1] != '+') {
         		if(j - 1 == 0) {
-        			length++;
-        			break;
+        			return ++length;
         		}
         		visited[i][j-1] = true;
         		que.offer(new int[] {i, j-1, length+1});
         	}
-        	if(i == 0 || j == 0 || i == maze.length-1 || j == maze[0].length-1) {
-        		length++;
-        		break;
-        	}
         }
-        System.out.println("=======");
-        return length == 0 ? -1 : length;
+        return -1;
     }
-	
-	private void printCharMap(char[][] maze) {
-		for(char[] i : maze) {
-			System.out.println(Arrays.toString(i));
-		}
-	}
 	
 }

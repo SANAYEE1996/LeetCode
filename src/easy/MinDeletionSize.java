c9d0e2f1247795257cd2eb09b688ad31592b245f
SANAYEE1996 <dudtkd0219@gmail.com>
2023-01-03 12:23:54 +0900

    after sleeping

--- src/easy/MinDeletionSize.java
+++ src/easy/MinDeletionSize.java
@@ -2,7 +2,23 @@ package easy;
 
 public class MinDeletionSize {
 	public int minDeletionSize(String[] strs) {
-        
-		return 0;
+        int columnLength = strs[0].length();
+        char beforeChar = 0;
+        int answer = 0;
+        int middleCount = 0;
+        for(int i = 0; i < columnLength; i++) {
+        	middleCount = 0;
+        	for(int j = 0; j < strs.length; j++) {
+        		if(beforeChar > strs[j].charAt(i)) {
+        			break;
+        		}
+        		middleCount++;
+        		beforeChar = strs[j].charAt(i);
+        	}
+        	if(middleCount == strs.length) {
+        		answer++;
+        	}
+        }
+		return answer;
     }
 }
--- src/easy/test/MinDeletionSizeTest.java
+++ src/easy/test/MinDeletionSizeTest.java
@@ -18,7 +18,17 @@ class MinDeletionSizeTest {
 	
 	@Test
 	void test() {
-		assertEquals(0,md.minDeletionSize(new String[] {}));
+		assertEquals(1,md.minDeletionSize(new String[] {"cba","daf","ghi"}));
+	}
+	
+	@Test
+	void test01() {
+		assertEquals(0,md.minDeletionSize(new String[] {"a","b"}));
+	}
+	
+	@Test
+	void test02() {
+		assertEquals(3,md.minDeletionSize(new String[] {"zyx","wvu","tsr"}));
 	}
 
 }

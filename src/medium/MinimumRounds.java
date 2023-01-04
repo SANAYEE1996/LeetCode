ddf1a924dc90dbe7868bc2c2f1f71e8e9cd3f36b
SANAYEE <dudtkd0219@gmail.com>
2023-01-04 20:17:27 +0900

    130ms faster than 9.22% // 103.6 mb less than 48.7 %

--- src/medium/MinimumRounds.java
+++ src/medium/MinimumRounds.java
@@ -1,11 +1,9 @@
 package medium;
 
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class MinimumRounds {
 	public int minimumRounds(int[] tasks) {
-		System.out.println("input : " +Arrays.toString(tasks));
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i : tasks) {
         	if(!map.containsKey(i)) {
@@ -13,37 +11,25 @@ public class MinimumRounds {
         	}
         	map.put(i, map.get(i)+1);
         }
-        printMap(map);
         int answer = 0;
-        int key = 0;
+        int value = 0;
         for(int i : map.keySet()) {
-        	key = i;
-        	while(true) {
-	        	if(map.get(key) == 1) {
-	        		answer = -1;
-	        		break;
-	        	}
-	        	else if(map.get(key) == 0) {
-	        		break;
-	        	}
-	        	else if(map.get(key) == 2){
-	        		map.put(key, map.get(key)-2);
-	        	}
-	        	else {
-	        		map.put(key, map.get(key)-3);
-	        	}
-	        	answer++;
+        	value = map.get(i);
+        	if(value== 1) {
+        		answer = -1;
+        		break;
         	}
-        	if(answer == -1) {
-        		return -1;
+        	else if(value%3 == 2 || value%3 == 1){
+        		answer += (value/3) + 1;
+        	}
+        	else if(value%3 == 0){
+        		answer += (value/3);
         	}
         }
+        if(answer == -1) {
+    		return -1;
+    	}
 		return answer;
     }
 	
-	private void printMap(HashMap<Integer, Integer> map) {
-		for(int i : map.keySet()) {
-        	System.out.println("key : "+i+" // value : " +map.get(i));
-        }
-	}
 }

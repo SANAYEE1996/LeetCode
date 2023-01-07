934f8fa1adf05dc5fb3140d10c9833238d55bdfc
SANAYEE <dudtkd0219@gmail.com>
2023-01-07 23:54:21 +0900

    2ms faster than 89.77 % // 62.3 MB less than 88.8%

--- src/medium/CanCompleteCircuit.java
+++ src/medium/CanCompleteCircuit.java
@@ -2,6 +2,35 @@ package medium;
 
 public class CanCompleteCircuit {
 	public int canCompleteCircuit(int[] gas, int[] cost) {
-        return 0;
+		
+		int[] gasArray = new int[gas.length];
+		for(int i = 0; i < gasArray.length; i++) {
+			gasArray[i] = gas[i] - cost[i];
+		}
+		int answer = -1;
+		int sum = 0;
+		boolean isFirstOverZero = true;
+		for(int i = 0; i < gasArray.length; i++) {
+			sum += gasArray[i];
+			if(sum < 0) {
+				sum = 0;
+				answer = -1;
+				isFirstOverZero = true;
+			}
+			else {
+				if(isFirstOverZero) {
+					isFirstOverZero = false;
+					answer = i;
+				}
+			}
+		}
+		for(int i = 0; i <= answer-1; i++) {
+			sum += gasArray[i];
+			if(sum < 0) {
+				answer = -1;
+				break;
+			}
+		}
+        return answer;
     }
 }
--- src/medium/test/CanCompleteCircuitTest.java
+++ src/medium/test/CanCompleteCircuitTest.java
@@ -18,9 +18,23 @@ class CanCompleteCircuitTest {
 	
 	@Test
 	void test() {
-		int[] gas = new int[5];
-		int[] cost = new int[5];
-		assertEquals(0,ccc.canCompleteCircuit(gas, cost));
+		int[] gas = {1,2,3,4,5};
+		int[] cost = {3,4,5,1,2};
+		assertEquals(3,ccc.canCompleteCircuit(gas, cost));
+	}
+	
+	@Test
+	void test02() {
+		int[] gas = {2,3,4};
+		int[] cost = {3,4,3};
+		assertEquals(-1,ccc.canCompleteCircuit(gas, cost));
+	}
+	
+	@Test
+	void test03() {
+		int[] gas = {2,3,4,1};
+		int[] cost = {3,4,3,5};
+		assertEquals(-1,ccc.canCompleteCircuit(gas, cost));
 	}
 
 }

e4977fba488529a28b1dd2f57f7c99c382f01617
SANAYEE <dudtkd0219@gmail.com>
2023-01-08 20:07:41 +0900

    .

--- src/medium/MaximumSwap.java
+++ src/medium/MaximumSwap.java
@@ -2,22 +2,16 @@ package medium;
 
 public class MaximumSwap {
 	public int maximumSwap(int num) {
-        String numString = String.valueOf(num);
+        String s = String.valueOf(num);
         int maxValue = num;
-        String exam = "";
-        for(int i = 0; i < numString.length()-1; i++) {
-        	for(int j = i+1; j < numString.length(); j++) {
-        		exam = swapNumber(numString, i, j);
-        		maxValue = Math.max(maxValue, Integer.parseInt(exam));
+        for(int i = 0; i < s.length()-1; i++) {
+        	for(int j = i+1; j < s.length(); j++) {
+        		maxValue = Math.max(maxValue, Integer.parseInt(s.substring(0, i) + s.charAt(j) + s.substring(i+1, j) + s.charAt(i) + s.substring(j+1)));
         	}
         }
 		return maxValue;
     }
 	
-	private String swapNumber(String s, int i, int j) {
-		s = s.substring(0, i) + s.charAt(j) + s.substring(i+1, j) + s.charAt(i) + s.substring(j+1);
-		return s;
-	}
 	
 	public static void main(String[] args) {
 		MaximumSwap s = new MaximumSwap();

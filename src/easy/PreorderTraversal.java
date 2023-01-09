458b833dabbd4fe4e3dc08f9fcbee2c922534b21
SANAYEE1996 <dudtkd0219@gmail.com>
2023-01-09 12:18:37 +0900

    default setting

---
+++ src/easy/PreorderTraversal.java
@@ -0,0 +1,12 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PreorderTraversal {
+	public List<Integer> preorderTraversal(TreeNode root) {
+		List<Integer> answerList = new ArrayList<>();
+		
+		return answerList;
+    }
+}

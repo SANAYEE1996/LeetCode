6809e8b27fc3ffde72f7fe62d5c53fa4cd66e5e4
SANAYEE1996 <dudtkd0219@gmail.com>
2023-01-09 12:24:37 +0900

    0 ms faster than 100% // 40.7 MB less than 62.3 %

--- src/easy/PreorderTraversal.java
+++ src/easy/PreorderTraversal.java
@@ -6,7 +6,19 @@ import java.util.List;
 public class PreorderTraversal {
 	public List<Integer> preorderTraversal(TreeNode root) {
 		List<Integer> answerList = new ArrayList<>();
-		
+		putList(answerList, root);
 		return answerList;
     }
+	
+	private void putList(List<Integer> list, TreeNode node) {
+		if(node != null) {
+			list.add(node.val);
+			if(node.left != null) {
+				putList(list, node.left);
+			}
+			if(node.right != null) {
+				putList(list, node.right);
+			}
+		}
+	}
 }

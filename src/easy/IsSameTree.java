c94f7a23cc4bfc4cbf1155faf7c8bba7909afafe
SANAYEE <dudtkd0219@gmail.com>
2023-01-10 19:20:37 +0900

    0 ms faster than 100 % // 39.5 MB less than 94.15 %

--- src/easy/IsSameTree.java
+++ src/easy/IsSameTree.java
@@ -1,23 +1,29 @@
 package easy;
 
 public class IsSameTree {
+	
+	private boolean isSameTreeFlag = true;
+	
 	public boolean isSameTree(TreeNode p, TreeNode q) {
-		if(p != null && q != null) {
-			if(p.val == q.val) {
-				return isSameTree(p.right, q.right);
-			}
-		}
-        return false;
+		gogo(p, q);
+        return isSameTreeFlag;
     }
 	
 	private void gogo(TreeNode p, TreeNode q) {
 		if(p != null && q != null) {
 			if(p.val == q.val) {
 				isSameTree(p.right, q.right);
+				isSameTree(p.left, q.left);
+			}
+			else {
+				isSameTreeFlag = false;
 			}
 		}
 		else if(p == null && q == null) {
 			return;
+		}
+		else {
+			isSameTreeFlag = false;
 		}
 	}
 }

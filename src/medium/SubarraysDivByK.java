148e219d83b71f8212a88967bd6d0923d40f7a52
SANAYEE1996 <dudtkd0219@gmail.com>
2023-01-19 12:14:14 +0900

    이렇게 하면 안되는 예 .. time limit exceed

---
+++ src/medium/SubarraysDivByK.java
@@ -0,0 +1,17 @@
+package medium;
+
+public class SubarraysDivByK {
+	public int subarraysDivByK(int[] nums, int k) {
+        int answer = 0;
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+        	sum = nums[i];
+        	if(Math.abs(sum) % k == 0) answer++;
+        	for(int j = i+1; j < nums.length; j++) {
+        		sum += nums[j];
+        		if(Math.abs(sum) % k == 0) answer++;
+        	}
+        }
+		return answer;
+    }
+}

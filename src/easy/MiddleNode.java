4b887b82451d69adb1323a1c1326201143e15053
SANAYEE <dudtkd0219@gmail.com>
2023-01-28 13:39:40 +0900

    0 ms, faster than 100.00% // 39.9 MB, less than 67.97%

--- src/easy/MiddleNode.java
+++ src/easy/MiddleNode.java
@@ -1,9 +1,24 @@
 package easy;
 
+import java.util.ArrayList;
+
 public class MiddleNode {
 	public ListNode middleNode(ListNode head) {
 		ListNode answer = new ListNode();
-		
+		ArrayList<Integer> list = new ArrayList<>();
+		while(head != null) {
+			list.add(head.val);
+			head = head.next;
+		}
+		ListNode copy = answer;
+		for(int i = list.size()/2; i < list.size(); i++) {
+			copy.val = list.get(i);
+			if(i == list.size()-1) {
+				break;
+			}
+			copy.next = new ListNode();
+			copy = copy.next;
+		}
 		return answer;
     }
 }

9f50c3a96c8a6f58dcedb03f5fc430c3835a1989
SANAYEE <dudtkd0219@gmail.com>
2023-01-30 19:27:54 +0900

    11 ms, faster than 21.99% // 40.2 MB, less than 98.34%

--- src/easy/LongestPalindrome.java
+++ src/easy/LongestPalindrome.java
@@ -1,5 +1,7 @@
 package easy;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class LongestPalindrome {
@@ -13,22 +15,33 @@ public class LongestPalindrome {
         	map.put(s.charAt(i), map.get(s.charAt(i))+1);
         }
         
-        boolean isOnlyOneCharacter = false;
         
         int answer = 0;
+        
+        ArrayList<Integer> oddList = new ArrayList<>();
+        ArrayList<Integer> evenList = new ArrayList<>();
+        
         for(Character key : map.keySet()) {
-        	if(map.get(key) == 1 && !isOnlyOneCharacter) {
-        		answer += 1;
-        		isOnlyOneCharacter = true;
+        	if(map.get(key) % 2 == 0) {
+        		evenList.add(map.get(key));
         	}
-        	else if(map.get(key) % 2 == 0) {
-        		answer += map.get(key);
-        	}
-        	else if(map.get(key) % 2 != 0) {
-        		answer += map.get(key)-1;
+        	else {
+        		oddList.add(map.get(key));
         	}
         }
-		
+        
+        for(int i : evenList) {
+        	answer += i;
+        }
+        
+        if(!oddList.isEmpty()) {
+	        Collections.sort(oddList);
+	        
+	        for(int i = 0; i < oddList.size()-1; i++) {
+	        	answer += (oddList.get(i)-1);
+	        }
+	        answer += oddList.get(oddList.size()-1);
+        }
 		return answer;
     }
 	
@@ -37,5 +50,6 @@ public class LongestPalindrome {
 		
 		System.out.println(s.longestPalindrome("abccccdd"));
 		System.out.println(s.longestPalindrome("a"));
+		System.out.println(s.longestPalindrome("ccc"));
 	}
 }

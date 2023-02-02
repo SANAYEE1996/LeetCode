6e16224e1235989fdcc57c94198315fc0e354967
SANAYEE <dudtkd0219@gmail.com>
2023-02-02 19:33:45 +0900

    time limit exceeded

--- src/easy/MinCostClimbingStairs.java
+++ src/easy/MinCostClimbingStairs.java
@@ -1,26 +1,36 @@
 package easy;
 
 public class MinCostClimbingStairs {
+	
+	private int minSum;
+	
 	public int minCostClimbingStairs(int[] cost) {
-        int answer = 1;
-		int sum = 0;
-        
-		for(int i = -1; i < cost.length; i++) {
-			if(i+2 < cost.length) {
-				if(cost[i+1] < cost[i+2]) {
-					
-				}
-			}
-			else {
-				if(i + 1 < cost.length) {
-					
-				}
-				else {
-					
-				}
-			}
+		minSum = Integer.MAX_VALUE;
+        gogo(cost, -1, 0, 1);
+		return minSum;
+    }
+	
+	private void gogo(int[] cost, int startIndex, int sum, int count) {
+		
+		if(startIndex + 1 < cost.length) {
+			gogo(cost, startIndex+1, sum + cost[startIndex + 1], count+1);
+		}
+		else {
+			minSum = (sum < minSum) ? sum : minSum;
 		}
 		
-		return 0;
-    }
+		if(startIndex + 2 < cost.length) {
+			gogo(cost, startIndex+2, sum + cost[startIndex + 2], count+1);
+		}
+		else {
+			minSum = (sum < minSum) ? sum : minSum;
+		}
+	}
+	
+	public static void main(String[] args) {
+		MinCostClimbingStairs s = new MinCostClimbingStairs();
+		
+		System.out.println(s.minCostClimbingStairs(new int[] {10,15,20}));
+		System.out.println(s.minCostClimbingStairs(new int[] {1,100,1,1,1,100,1,1,100,1}));
+	}
 }

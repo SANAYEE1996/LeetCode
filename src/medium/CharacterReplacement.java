215426ad935f7c713aef1394bf78c205db86c005
SANAYEE <dudtkd0219@gmail.com>
2023-02-09 18:45:01 +0900

    7 ms, faster than 69.45% // 42.2 MB, less than 57.35%

--- src/medium/CharacterReplacement.java
+++ src/medium/CharacterReplacement.java
@@ -2,32 +2,32 @@ package medium;
 
 public class CharacterReplacement {
 	public int characterReplacement(String s, int k) {
-		int answer = 1;
-		
-		int length = 1;
-		char lastChar = s.charAt(0);
-		int changeCount = k;
-		
-		for(int i = 1; i < s.length(); i++) {
-			if(s.charAt(i) == lastChar) {
-				length++;
-			}
-			else {
-				if(changeCount > 0) {
-					length++;
-					changeCount--;
-				}
-				else {
-					answer = Math.max(answer, length);
-					length = 1;
-					lastChar = s.charAt(i);
-					changeCount = k;
-				}
-			}
-		}
-		
-		
-        return Math.max(answer, length);
+        int start = 0;
+        int[] frequencyMap = new int[26];
+        int maxFrequency = 0;
+        int longestSubstringLength = 0;
+
+        for (int end = 0; end < s.length(); end += 1) {
+        	
+            int currentChar = s.charAt(end) - 'A';
+
+            frequencyMap[currentChar] += 1;
+
+            maxFrequency = Math.max(maxFrequency, frequencyMap[currentChar]);
+
+            Boolean isValid = (end + 1 - start - maxFrequency <= k);
+            if (!isValid) {
+                int outgoingChar = s.charAt(start) - 'A';
+
+                frequencyMap[outgoingChar] -= 1;
+
+                start += 1;
+            }
+
+            longestSubstringLength = end + 1 - start;
+        }
+
+        return longestSubstringLength;
     }
 	
 	public static void main(String[] args) {

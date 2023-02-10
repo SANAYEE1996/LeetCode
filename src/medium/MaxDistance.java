17cd92661343321fea8c94a078dffabfc2ea3ef9
SANAYEE <dudtkd0219@gmail.com>
2023-02-11 00:11:00 +0900

    .

--- src/medium/MaxDistance.java
+++ src/medium/MaxDistance.java
@@ -9,23 +9,32 @@ public class MaxDistance {
 		int[][] map = new int[length][length];
 		for(int i = 0; i < length; i++) {
 			for(int j = 0; j < length; j++) {
-				if(grid[i][j] == 1) {continue;}
-				fillCross(map, grid, i, j);
-				if(map[i][j] == 1) {continue;}
-				fillAxe(map, grid, i, j);
-				if(map[i][j] == 2) {continue;}
-				fillCross(map, i, j);
+				if(grid[i][j] == 1) {
+					goBack(map, grid, i, j-1);
+					gogo(map, grid, i, j+1);
+					break;					
+				}
 			}
 		}
 		
+		for(int j = 0; j < length; j++) {
+			for(int i = 0; i < length; i++) {
+				if(grid[i][j] == 1) {
+					goUpBack(map, grid, i-1, j);
+					goDowngo(map, grid, i+1, j);
+					break;					
+				}
+			}
+		}
+		for(int[] i : map) {
+			System.out.println(Arrays.toString(i));
+		}
+		System.out.println("=============================================");
 		for(int i = 0; i < length; i++) {
 			for(int j = 0; j < length; j++) {
-				if(map[i][j] == 0) {continue;}
-				fillCross(map, grid, i, j);
-				if(map[i][j] == 1) {continue;}
-				fillAxe(map, grid, i, j);
-				if(map[i][j] == 2) {continue;}
-				fillCross(map, i, j);
+				if(grid[i][j] != 1) {
+					fillCross(map, i, j);
+				}
 			}
 		}
 		
@@ -36,33 +45,85 @@ public class MaxDistance {
         return answer;
     }
 	
-	private void fillCross(int[][] map, int[][] grid, int i, int j) {
-		if(isUpOne(grid, i, j) || isDownOne(grid, i, j) || isLeftOne(grid, i, j) || isRightOne(grid, i, j)) {
-			map[i][j] = 1;
+	private void goBack(int[][] map, int[][] grid, int i, int j) {
+		int value = 0;
+		for(; j >= 0; j--) {
+			if(isUpOne(grid, i, j) || isDownOne(grid, i, j)) {
+				value = 1;
+			}
+			else {
+				value++;
+			}
+			map[i][j] = value;
 		}
 	}
 	
-	private void fillAxe(int[][] map, int[][] grid, int i, int j) {
-		if(upright(grid, i, j) || downleft(grid, i, j) || downright(grid, i, j) || upleft(grid, i, j)) {
-			map[i][j] = 2;
+	private void goUpBack(int[][] map, int[][] grid, int i, int j) {
+		int value = 0;
+		for(; i >= 0; i--) {
+			if(isLeftOne(grid, i, j) || isRightOne(grid, i, j)) {
+				value = 1;
+			}
+			else {
+				value++;
+			}
+			map[i][j] = value;
+		}
+	}
+	
+	private void gogo(int[][] map, int[][] grid, int i, int j) {
+		int value = 0;
+		for(; j < map.length; j++) {
+			if(isRightOne(grid, i, j)) {
+				map[i][j] = 1;
+				gogo(map, grid, i, j+2);
+				break;
+			}
+			if(isUpOne(grid, i, j) || isDownOne(grid, i, j)) {
+				value = 1;
+			}
+			else {
+				value++;
+			}
+			map[i][j] = value;			
+		}
+	}
+	
+	private void goDowngo(int[][] map, int[][] grid, int i, int j) {
+		int value = 0;
+		for(; i < map.length; i++) {
+			if(isDownOne(grid, i, j)) {
+				map[i][j] = 1;
+				goDowngo(map, grid, i+2, j);
+				break;
+			}
+			if(isLeftOne(grid, i, j) || isRightOne(grid, i, j)) {
+				value = 1;
+			}
+			else {
+				value++;
+			}
+			map[i][j] = value;			
 		}
 	}
 	
 	private void fillCross(int[][] map, int i, int j) {
 		int minValue = Integer.MAX_VALUE;
-		if(i > 0 && map[i-1][j] != 0) {
+		if(i > 0 ) {
 			minValue = Math.min(minValue, map[i-1][j]);
 		}
-		if(j > 0 && map[i][j-1] != 0) {
+		if(j > 0 ) {
 			minValue = Math.min(minValue, map[i][j-1]);
 		}
-		if(i < map.length-1 && map[i+1][j] != 0) {
+		if(i < map.length-1) {
 			minValue = Math.min(minValue, map[i+1][j]);
 		}
-		if(j < map.length-1 && map[i][j+1] != 0) {
+		if(j < map.length-1) {
 			minValue = Math.min(minValue, map[i][j+1]);
 		}
-		map[i][j] = minValue+1;
+		if(minValue != Integer.MAX_VALUE) {
+			map[i][j] = minValue+1;
+		}
 	}
 	
 	private boolean isUpOne(int[][] grid, int i, int j) {
@@ -81,21 +142,6 @@ public class MaxDistance {
 		return (j < grid.length-1 && grid[i][j+1] == 1);
 	}
 	
-	private boolean upright(int[][] grid, int i, int j) {
-		return (i > 0 && j < grid.length-1 && grid[i-1][j+1] == 1);
-	}
-	
-	private boolean downright(int[][] grid, int i, int j) {
-		return (i < grid.length-1 && j < grid.length-1 && grid[i+1][j+1] == 1);
-	}
-	
-	private boolean downleft(int[][] grid, int i, int j) {
-		return (i < grid.length-1 && j > 0 && grid[i+1][j-1] == 1);
-	}
-	
-	private boolean upleft(int[][] grid, int i, int j) {
-		return (i > 0 && j > 0 && grid[i-1][j-1] == 1);
-	}
 	
 	public static void main(String[] args) {
 		int[][] grid = {{1,0,1},{0,0,0},{1,0,1}};

057e75d7c93858289d73d49c34903534c6cd8382
SANAYEE <dudtkd0219@gmail.com>
2023-02-14 21:48:56 +0900

    2 ms, faster than 61.76% // 41.8 MB, less than 59.55%

--- src/easy/AddBinary.java
+++ src/easy/AddBinary.java
@@ -3,47 +3,71 @@ package easy;
 
 public class AddBinary {
 	public String addBinary(String a, String b) {
-        try {
-			int aNumber = getIntegerFromBinaryString(a);
-			int bNumber = getIntegerFromBinaryString(b);
-			return getStringFromInteger(aNumber + bNumber);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return "Error";
+		a = getBack(a);
+		b = getBack(b);
+		if(a.length() <= b.length()) {
+			return gogo(a, b);
 		}
+		return gogo(b, a);
     }
 	
-	private int getIntegerFromBinaryString(String a) throws Exception{
-		int numb = 0;
-		int twoSquare = -1;
-		for(int i = a.length()-1; i >= 0; i--) {
-			twoSquare++;
-			if(a.charAt(i) == '0') {
-				continue;
-			}
-			else if(a.charAt(i) == '1') {
-				numb += Math.pow(2, twoSquare);
-			}
-			else {
-				throw new Exception("It is not binary String");
-			}
-		}
-		return numb;
-	}
-	
-	private String getStringFromInteger(int a) {
-		if(a == 0) return "0";
+	private String getBack(String a) {
 		StringBuilder sb = new StringBuilder();
-		while(a != 0) {
-			if(a%2 == 0) {
-				sb.insert(0, 0);
-			}
-			else {
-				sb.insert(0, 1);
-			}
-			a/=2;
+		for(int i = a.length()-1; i>= 0; i--) {
+			sb.append(a.charAt(i));
 		}
 		return sb.toString();
 	}
-
+	
+	private String gogo(String a, String b) {
+		StringBuilder sb = new StringBuilder();
+		int pivot = 0;
+		for(int i = 0; i < a.length(); i++) {
+			pivot = getPivot(a.charAt(i), b.charAt(i), pivot, sb);
+		}
+		for(int i = a.length(); i < b.length(); i++) {
+			pivot = getPivot(b.charAt(i), pivot, sb);
+		}
+		if(pivot == 0) {
+			return sb.toString();
+		}
+		sb.insert(0, 1);
+		return sb.toString();
+	}
+	
+	private int getPivot(char a, char b, int pivot, StringBuilder sb) {
+		if(a + b == 96) { 
+			sb.insert(0, pivot);
+			return 0;
+		}
+		else if(a + b == 98) { 
+			if(pivot == 1) {
+				sb.insert(0, 1);
+				return pivot;
+			}
+			sb.insert(0, 0);
+			return 1;
+		}
+		else { 
+			if(pivot == 1) {
+				sb.insert(0, 0);
+				return pivot;
+			}
+			sb.insert(0, 1);
+			return 0;
+		}
+	}
+	
+	private int getPivot(char a, int pivot, StringBuilder sb) {
+		if(a == '1' && pivot == 1) {
+			sb.insert(0, 0);
+			return 1;
+		}
+		else if(a == '0' && pivot == 0) {
+			sb.insert(0, 0);
+			return 0;
+		}
+		sb.insert(0, 1);
+		return 0;
+	}
 }

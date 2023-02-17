f09fe77dce2890198d966f43d50e6202274bb452
SANAYEE <dudtkd0219@gmail.com>
2023-02-17 22:01:59 +0900

    .

--- src/medium/CountFairPairs.java
+++ src/medium/CountFairPairs.java
@@ -1,16 +1,44 @@
 package medium;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class CountFairPairs {
 	
 	public long countFairPairs(int[] nums, int lower, int upper) {
+		HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
         Arrays.sort(nums);
-        binarySearch(nums,lower);
-		return 0;
+        System.out.println(Arrays.toString(nums));
+        for(int i = 0; i < nums.length; i++) {
+        	if(!map.containsKey(nums[i])) {
+        		map.put(nums[i], new ArrayList<>());
+        	}
+        	map.get(nums[i]).add(i);
+        }
+        int minusLower = 0;
+        int minusUpper = 0;
+        int lowIndex = 0;
+        int upIndex = 0;
+        long answer = 0;
+        long part = 0;
+        for(int key : map.keySet()) {
+        	minusLower = lower - key;
+        	minusUpper = upper - key;
+        	lowIndex = binarySearch(nums, minusLower);
+        	upIndex = binarySearch(nums, minusUpper);
+        	System.out.println("key : " +key);
+        	upIndex = (nums[upIndex] > minusUpper) ? upIndex-1 : upIndex;
+        	lowIndex = (nums[lowIndex] < minusLower) ? lowIndex+1 : lowIndex;
+        	System.out.println("minusLower : " +minusLower + " minusUpper : " +minusUpper);
+        	System.out.println("lowIndex : " +lowIndex + " upIndex : " +upIndex);
+        	part = (long)((long)upIndex - (long)lowIndex + (long)1);
+        	answer += part;
+        }
+		return answer;
     }
 	
-	private void binarySearch(int[] array, int target){
+	private int binarySearch(int[] array, int target){
 		int low = 0;
 		int high = array.length-1;
 		int mid = (low+high)/2;
@@ -27,16 +55,12 @@ public class CountFairPairs {
 				mid = (low+high)/2;
 			}
 		}
-		System.out.println("-----mid : " +mid + " is find ? " +(array[mid] == target));
-		
+		return mid;
 	}
 	
 	public static void main(String[] args) {
 		CountFairPairs s = new CountFairPairs();
-		//System.out.println(s.countFairPairs(new int[] {0,1,4,4,5,7}, 3, 6));
-		s.binarySearch(new int[] {0,1,4,4,5,7}, 2);
-		s.binarySearch(new int[] {0,1,4,4,5,7}, 3);
-		s.binarySearch(new int[] {0,1,4,4,5,7}, 6);
-		s.binarySearch(new int[] {0,1,4,4,5,7}, 7);
+		System.out.println(s.countFairPairs(new int[] {0,1,4,4,5,7}, 3, 6));
+		System.out.println(s.countFairPairs(new int[] {0,1,4,4,5,7}, 2, 6));
 	}
 }

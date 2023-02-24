83ce8c98f9efb7431a06d08a66d74cc193a7c9b8
SANAYEE1996 <dudtkd0219@gmail.com>
2023-02-24 10:09:53 +0900

    i cannot understand problem

--- src/hard/MinimumDeviation.java
+++ src/hard/MinimumDeviation.java
@@ -1,18 +1,51 @@
 package hard;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class MinimumDeviation {
 	public int minimumDeviation(int[] nums) {
         ArrayList<Integer> oddList = new ArrayList<>();
         ArrayList<Integer> evenList = new ArrayList<>();
-        for(int i : nums) {
+        putIntegerToList(nums, oddList, evenList);
+        Collections.sort(oddList);
+        Collections.sort(evenList);
+        int minOdd = 0;
+        int maxEven = 0;
+        if(!oddList.isEmpty() && !evenList.isEmpty()) {
+        	minOdd = oddList.get(0)*2;
+        	maxEven = getOddFromEven(evenList.get(evenList.size()-1));
+        	return Math.abs(maxEven-minOdd);
+        }
+        else if(oddList.isEmpty()) {
+        	minOdd = evenList.get(0);
+        	maxEven = getOddFromEven(evenList.get(evenList.size()-1));
+        	return Math.abs(maxEven-minOdd);
+        }
+        else if(evenList.isEmpty()) {
+        	minOdd = oddList.get(0)*2;
+        	maxEven = oddList.get(1);
+        	return Math.abs(maxEven-minOdd);
+        }
+        
+		return 0;
+    }
+	
+	private void putIntegerToList(int[] nums, ArrayList<Integer> oddList, ArrayList<Integer> evenList) {
+		for(int i : nums) {
         	if(i % 2 == 0) {
         		evenList.add(i);
         		continue;
         	}
         	oddList.add(i);
         }
-		return 0;
-    }
+	}
+	
+	private int getOddFromEven(int even) {
+		while(even%2 == 0) {
+			even /= 2;
+		}
+		return even;
+	}
+	
 }

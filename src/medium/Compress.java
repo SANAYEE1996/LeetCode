2a0275533d51862123aeeee8beebfcbac56674ab
SANAYEE <dudtkd0219@gmail.com>
2023-03-02 19:34:12 +0900

    1 ms, faster than 98.42% // 42.3 MB, less than 29.22%

--- src/medium/Compress.java
+++ src/medium/Compress.java
@@ -1,16 +1,12 @@
 package medium;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Compress {
 	public int compress(char[] chars) {
 		int answer = 0;
 		char beforeChar = chars[0];
 		char nowChar = '0';
 		int duplicateCount = 1;
-		ArrayList<Character> charList = new ArrayList<>();
-		ArrayList<Integer> countList = new ArrayList<>();
+		StringBuilder sb = new StringBuilder();
 		for(int i = 1; i < chars.length; i++){
 			nowChar = chars[i];
 			if(beforeChar == nowChar){
@@ -18,36 +14,28 @@ public class Compress {
 				continue;
 			}
 			answer++;
-			charList.add(beforeChar);
-			countList.add(duplicateCount);
+			sb.append(beforeChar);
+			if(duplicateCount > 1) {
+				putIntegerToStringBuilder(duplicateCount, sb);
+			}
 			answer += duplicateCount == 1 ? 0 : getSizeFromInteger(duplicateCount);
 			duplicateCount = 1;
 			beforeChar = nowChar;
 		}
 		answer++;
 		answer += duplicateCount == 1 ? 0 : getSizeFromInteger(duplicateCount);
-		charList.add(nowChar == '0' ? beforeChar : nowChar);
-		countList.add(duplicateCount);
-		System.out.println(charList);
-		System.out.println(countList);
-		chars = new char[answer];
-		int value = 0;
-		int listIndex = 0;
-		for(int i = 0; i < chars.length; i++) {
-			chars[i] = charList.get(listIndex);
-			value = countList.get(listIndex);
-			if(value == 1) {continue;}
-			char[] exam = intToCharArray(value);
-			int index = 0;
-			i++;
-			for(; i < chars.length; i++) {
-				if(index == exam.length) {break;}
-				chars[i] = exam[index];
-				index++;
+		if(nowChar == '0') {
+			sb.append(beforeChar);
+		}else {
+			sb.append(nowChar);
+			if(duplicateCount > 1) {
+				putIntegerToStringBuilder(duplicateCount, sb);
 			}
-			listIndex++;
 		}
-		System.out.println(Arrays.toString(chars));
+		String answerString = sb.toString();
+		for(int i = 0; i < answer; i++) {
+			chars[i] = answerString.charAt(i);
+		}
 		return answer;
 	}
 
@@ -60,20 +48,16 @@ public class Compress {
 		return size;
 	}
 	
-	private char[] intToCharArray(int num) {
+	private void putIntegerToStringBuilder(int num, StringBuilder sb) {
 		int size = getSizeFromInteger(num);
-		char[] charArray = new char[size];
-		int index = 0;
 		size--;
 		int value = 0;
 		while(size >= 0) {
 			value = num/(int)Math.pow(10, size);
-			charArray[index] = (char) (value+48);
+			sb.append((char) (value+48));
 			num -= value * (int)Math.pow(10, size);
 			size--;
-			index++;
 		}
-		return charArray;
 	}
 	
 }

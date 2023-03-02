90718b318a0457f90731794771d26de65b3e7c20
SANAYEE1996 <dudtkd0219@gmail.com>
2023-03-02 12:31:29 +0900

    .

--- src/medium/Compress.java
+++ src/medium/Compress.java
@@ -1,6 +1,7 @@
 package medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Compress {
 	public int compress(char[] chars) {
@@ -17,18 +18,36 @@ public class Compress {
 				continue;
 			}
 			answer++;
-			answer += getSizeFromInteger(duplicateCount);
 			charList.add(beforeChar);
 			countList.add(duplicateCount);
+			answer += duplicateCount == 1 ? 0 : getSizeFromInteger(duplicateCount);
 			duplicateCount = 1;
 			beforeChar = nowChar;
 		}
 		answer++;
-		answer += getSizeFromInteger(duplicateCount);
-		charList.add(nowChar);
+		answer += duplicateCount == 1 ? 0 : getSizeFromInteger(duplicateCount);
+		charList.add(nowChar == '0' ? beforeChar : nowChar);
 		countList.add(duplicateCount);
 		System.out.println(charList);
 		System.out.println(countList);
+		chars = new char[answer];
+		int value = 0;
+		int listIndex = 0;
+		for(int i = 0; i < chars.length; i++) {
+			chars[i] = charList.get(listIndex);
+			value = countList.get(listIndex);
+			if(value == 1) {continue;}
+			char[] exam = intToCharArray(value);
+			int index = 0;
+			i++;
+			for(; i < chars.length; i++) {
+				if(index == exam.length) {break;}
+				chars[i] = exam[index];
+				index++;
+			}
+			listIndex++;
+		}
+		System.out.println(Arrays.toString(chars));
 		return answer;
 	}
 
@@ -40,4 +59,21 @@ public class Compress {
 		}
 		return size;
 	}
+	
+	private char[] intToCharArray(int num) {
+		int size = getSizeFromInteger(num);
+		char[] charArray = new char[size];
+		int index = 0;
+		size--;
+		int value = 0;
+		while(size >= 0) {
+			value = num/(int)Math.pow(10, size);
+			charArray[index] = (char) (value+48);
+			num -= value * (int)Math.pow(10, size);
+			size--;
+			index++;
+		}
+		return charArray;
+	}
+	
 }
--- src/medium/test/CompressTest.java
+++ src/medium/test/CompressTest.java
@@ -18,8 +18,22 @@ class CompressTest {
 	
 	@Test
 	void test01() {
-		System.out.println(c.compress(new char[] {'a','a','a','a'}));
 		assertEquals(2,c.compress(new char[]{'a','a','a','a'}));
+	}
+	
+	@Test
+	void test02() {
+		assertEquals(6,c.compress(new char[]{'a','a','b','b','c','c','c'}));
+	}
+	
+	@Test
+	void test03() {
+		assertEquals(1,c.compress(new char[]{'a'}));
+	}
+	
+	@Test
+	void test04() {
+		assertEquals(4,c.compress(new char[]{'a','b','b','b','b','b','b','b','b','b','b','b','b'}));
 	}
 
 }

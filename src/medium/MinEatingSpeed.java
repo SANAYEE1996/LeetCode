6975b3bbb56c88fd70cd33faf2f2a41ebfdcc97c
SANAYEE1996 <dudtkd0219@gmail.com>
2023-03-09 08:57:00 +0900

    44 ms, faster than 20.37% // 43.7 MB, less than 23.00%

---
+++ src/medium/MinEatingSpeed.java
@@ -0,0 +1,26 @@
+package medium;
+
+public class MinEatingSpeed {
+	public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = 1;
+        for(int pile : piles){
+            right = Math.max(right, pile);
+        }
+        
+        while(left<right) {
+            int mid = left + (right-left)/2;
+            int hours = 0;
+            for(int pile: piles){
+                hours += Math.ceil(pile*1.0/mid);
+            }
+            if(hours<=h){
+                right = mid;
+            }
+            else {
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+}

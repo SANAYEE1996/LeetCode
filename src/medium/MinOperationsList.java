d829f6724ddac8f5eea1d6ef70496cb1d6b1de6d
SANAYEE <dudtkd0219@gmail.com>
2023-03-26 13:55:37 +0900

    dst

---
+++ src/medium/MinOperationsList.java
@@ -0,0 +1,12 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinOperationsList {
+	public List<Long> minOperations(int[] nums, int[] queries) {
+		List<Long> answerList = new ArrayList<>();
+		
+		return answerList;
+    }
+}

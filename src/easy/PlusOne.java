29c743389ec1e09ff3bf88de62936c14d8fb880b
SANAYEE <dudtkd0219@gmail.com>
2023-04-04 00:08:27 +0900

    0 ms, faster than 100.00% // 41.2 MB, less than 22.85%

--- src/easy/PlusOne.java
+++ src/easy/PlusOne.java
@@ -4,8 +4,14 @@ public class PlusOne {
 	public int[] plusOne(int[] digits) {
         int pivot = 1;  
         for(int i = digits.length-1; i>= 0; i--) {
-        	digits[i] = digits[i] + pivot >= 10 ? 0 : digits[i] + pivot;
-        	pivot = digits[i] + pivot >= 10 ? 1 : 0;
+        	if(digits[i] + pivot >= 10) {
+                digits[i] = digits[i] + pivot - 10;
+        		pivot = 1;
+        	}
+        	else {
+                digits[i] = digits[i] + pivot;
+        		pivot = 0;
+        	}
         }
         if(pivot == 0) {
         	return digits;
@@ -15,7 +21,6 @@ public class PlusOne {
         for(int i = 1; i < answer.length; i++) {
         	answer[i] = digits[i-1];
         }
-        
 		return answer;
     }
 }

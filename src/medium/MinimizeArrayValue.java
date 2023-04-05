a39bf29b6249a2c37c1e77df6eb0bd4b7aa4b4ca
SANAYEE1996 <dudtkd0219@gmail.com>
2023-04-05 17:07:21 +0900

    9 ms, faster than 74.55% // 62.7 MB, less than 7.17%

--- src/medium/MinimizeArrayValue.java
+++ src/medium/MinimizeArrayValue.java
@@ -2,20 +2,13 @@ package medium;
 
 public class MinimizeArrayValue {
 	public int minimizeArrayValue(int[] nums) {
-        int value = nums[0];
-        int max = nums[0];
-        
-        for(int i = nums.length-1; i >= 1; i--) {
-        	if(nums[i] > value) {
-        		max = nums[i];
-        		break;
-        	}
+		long answer = 0, prefixSum = 0;   
+
+        for (int i = 0; i < nums.length; ++i) {
+            prefixSum += nums[i];
+            answer = Math.max(answer, (prefixSum + i) / (i + 1));
         }
-        
-        int differ = max - value;
-        differ /= 2;
-        
-		
-		return Math.max(value+differ, max-differ);
+
+        return (int)answer;
     }
 }

8bab4c9694126b27dc1586cb2c0bf6c0176af7f2
SANAYEE1996 <dudtkd0219@gmail.com>
2023-04-18 13:48:23 +0900

    27 ms, faster than 55.00% // 51.3 MB, less than 75.50%

--- src/medium/FindMaxLength.java
+++ src/medium/FindMaxLength.java
@@ -1,19 +1,18 @@
 package medium;
 
+import java.util.HashMap;
+
 public class FindMaxLength {
 	public int findMaxLength(int[] nums) {
-		int maxlen = 0;
-        for (int start = 0; start < nums.length; start++) {
-            int zeroes = 0, ones = 0;
-            for (int end = start; end < nums.length; end++) {
-                if (nums[end] == 0) {
-                    zeroes++;
-                } else {
-                    ones++;
-                }
-                if (zeroes == ones) {
-                    maxlen = Math.max(maxlen, end - start + 1);
-                }
+		HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int maxlen = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count = count + (nums[i] == 1 ? 1 : -1);
+            if (map.containsKey(count)) {
+                maxlen = Math.max(maxlen, i - map.get(count));
+            } else {
+                map.put(count, i);
             }
         }
         return maxlen;

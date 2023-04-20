954593489c23b29e482145f0f75b04bba75b4a23
SANAYEE <dudtkd0219@gmail.com>
2023-04-20 21:43:03 +0900

    11 ms, faster than 6.02% // 41.3 MB, less than 12.65%

--- src/medium/SolveEquation.java
+++ src/medium/SolveEquation.java
@@ -1,49 +1,121 @@
 package medium;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class SolveEquation {
 	public String solveEquation(String equation) {
-		String[] strArray = equation.split("-|\\+|\\=");
-		int leftValue = 0;
-		String leftX = "";
+		ArrayList<String> list = new ArrayList<>();
+		for(int i = 0; i < equation.length(); i++){
+			if(isMark(equation.charAt(i))) {
+				list.add(equation.substring(i,i+1));
+				continue;
+			}
+			StringBuilder sb = new StringBuilder();
+			for(; i < equation.length(); i++) {
+				if(isMark(equation.charAt(i))) {
+					i--;
+					break;
+				}
+				sb.append(equation.charAt(i));
+			}
+			list.add(sb.toString());
+		}
 		int middleIndex = 0;
-		int rightValue = 0;
-		String rightX = "";
-		System.out.println(Arrays.toString(strArray));
-		for(int i = 0; i < strArray.length; i++){
-			if(strArray[i].equals("=")){
+		char status = '+';
+		int leftValue = 0, rightValue = 0;
+		String leftX = "", rightX = "";
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).equals("=")) {
 				middleIndex = i+1;
 				break;
 			}
-			if(strArray[i].equals("x")){
-				leftX += "x";
-			}else if(strArray[i].equals("-") || strArray[i].equals("+")){
+			else if(list.get(i).equals("+")) {
+				status = '+';
 				continue;
-			}else{
-				if((i > 0 && strArray[i-1].equals("+")) || i == 0){
-					leftValue += Integer.parseInt(strArray[i-1]);
-				}else{
-					leftValue -= Integer.parseInt(strArray[i-1]);
-				}
+			}
+			else if(list.get(i).equals("-")) {
+				status = '-';
+				continue;
+			}
+			else if(list.get(i).contains("x")) {
+				leftX = getX(leftX, list.get(i), status);
+				continue;
+			}
+			if(status == '+') {
+				leftValue += Integer.parseInt(list.get(i));
+			}
+			else {
+				leftValue -= Integer.parseInt(list.get(i));
 			}
 		}
-		for(int i = middleIndex; i < strArray.length; i++){
-			if(strArray[i].equals("x")){
-				rightX += "x";
-			}else if(strArray[i].equals("-") || strArray[i].equals("+")){
+		status = '+';
+		for(int i = middleIndex; i < list.size(); i++) {
+			if(list.get(i).equals("+")) {
+				status = '+';
 				continue;
-			}else{
-				if((i > middleIndex && strArray[i-1].equals("+")) || i == middleIndex){
-					rightValue += Integer.parseInt(strArray[i-1]);
-				}else{
-					rightValue -= Integer.parseInt(strArray[i-1]);
-				}
+			}
+			else if(list.get(i).equals("-")) {
+				status = '-';
+				continue;
+			}
+			else if(list.get(i).contains("x")) {
+				rightX = getX(rightX, list.get(i), status);
+				continue;
+			}
+			if(status == '+') {
+				rightValue += Integer.parseInt(list.get(i));
+			}
+			else {
+				rightValue -= Integer.parseInt(list.get(i));
 			}
 		}
-		System.out.println("leftValue : " +leftValue + "leftX : " +leftX);
-		System.out.println("rightValue : " +rightValue + "rightX : " +rightX);
-		return "";
-		
+		if(rightX.equals(leftX) && leftValue == rightValue) {
+			return "Infinite solutions";
+		}
+		String totalX = getX(leftX, rightX, '-');
+		int totalValue = rightValue - leftValue;
+		if(totalX.equals("")) {
+			return "No solution";
+		}
+		return "x="+String.valueOf(totalValue/getInteger(totalX));
+	}
+	
+	private boolean isMark(char a) {
+		return a == '+' || a == '-' || a == '=';
+	}
+	
+	private String getX(String a, String b, char status) {
+		int aInteger = getInteger(a);
+		int bInteger = getInteger(b);
+		int calculate = 0;
+		if(status == '+') {
+			calculate = aInteger + bInteger;
+		}
+		else {
+			calculate = aInteger - bInteger;
+		}
+		if(calculate == 0) {
+			return "";
+		}
+		else if(calculate == 1) {
+			return "x";
+		}
+		else if(calculate == -1) {
+			return "-x";
+		}
+		return String.valueOf(calculate) + "x";
+	}
+	
+	private int getInteger(String x) {
+		if(x.equals("")) {
+			return 0;
+		}
+		else if(x.length() == 1) {
+			return 1;
+		}
+		else if(x.equals("-x")) {
+			return -1;
+		}
+		return Integer.parseInt(x.substring(0, x.length()-1));
 	}
 }
--- src/medium/test/SolveEquationTest.java
+++ src/medium/test/SolveEquationTest.java
@@ -18,7 +18,32 @@ class SolveEquationTest {
 
 	@Test
 	void test() {
-		assertEquals("",se.solveEquation("x+5-3+x=6+x-2"));
+		assertEquals("x=2",se.solveEquation("x+5-3+x=6+x-2"));
+	}
+	
+	@Test
+	void test01() {
+		assertEquals("Infinite solutions",se.solveEquation("x=x"));
+	}
+	
+	@Test
+	void test02() {
+		assertEquals("x=0",se.solveEquation("2x=x"));
+	}
+	
+	@Test
+	void test03() {
+		assertEquals("x=10",se.solveEquation("19x-49-12x=11+x"));
+	}
+	
+	@Test
+	void test04() {
+		assertEquals("x=-30",se.solveEquation("9x-49-12x=11-x"));
+	}
+	
+	@Test
+	void test05() {
+		assertEquals("No solution",se.solveEquation("x=x+2"));
 	}
 
 }

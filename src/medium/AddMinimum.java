fd3cc8207f73fc6b1053fbc9c5c98578db8bdaf2
SANAYEE1996 <dudtkd0219@gmail.com>
2023-04-21 09:49:04 +0900

    3 ms, faster than 27.25% // 42.2 MB, less than 95.88%

--- src/medium/AddMinimum.java
+++ src/medium/AddMinimum.java
@@ -1,15 +1,14 @@
 package medium;
 
-import java.util.ArrayList;
-
 public class AddMinimum {
 	public int addMinimum(String word) {
-		ArrayList<String> list = new ArrayList<>();
 		
 		char beforeChar = word.charAt(0);
 		int differ = 0;
 		StringBuilder sb = new StringBuilder();
 		sb.append(beforeChar);
+		int answer = 0;
+		String part = "";
 		for(int i = 1; i < word.length(); i++){
 			differ = word.charAt(i) - beforeChar;
 			if(differ == 1 || differ == 2){
@@ -17,20 +16,19 @@ public class AddMinimum {
 				beforeChar = word.charAt(i);
 				continue;
 			}
-			list.add(sb.toString());
+			part = sb.toString();
+			if(!part.equals("")) {
+				answer += (3-part.length());
+			}
 			sb.setLength(0);
 			sb.append(word.charAt(i));
 			beforeChar = word.charAt(i);
 		}
-		list.add(sb.toString());
-		System.out.println(list);
-		int answer = 0;
-		for(String s : list){
-			if(s.equals("")){
-				continue;
-			}
-			answer += (3-s.length());
+		part = sb.toString();
+		if(!part.equals("")) {
+			answer += (3-part.length());
 		}
+		
 		return answer;
 	}
 }

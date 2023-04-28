b76eff1b098fac9fdac09d2bee6d06936a325b98
SANAYEE1996 <dudtkd0219@gmail.com>
2023-04-28 18:31:08 +0900

    방법을 제시

--- src/medium/IntegerReplacement.java
+++ src/medium/IntegerReplacement.java
@@ -1,49 +1,53 @@
 package medium;
 
+import java.util.ArrayList;
+
 public class IntegerReplacement {
 	
 	public int integerReplacement(int n) {
-		int answer = 0;
-		int left = 0;
-		int right = 0;
-		while(n != 1){
-			answer++;
-			if(n % 2 == 0){
-				n /= 2;
-			}
-			else{
-				if(n + 1 <= Integer.MAX_VALUE){
-					left = commonDivisorCount(n-1);
-					right = commonDivisorCount(n+1);
-					if(left < right){
-						n += 1;
-						continue;
-					}
-					n -= 1;
-					continue;
-				}
-				n -= 1;
-			}
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i = 0; i < 31; i++) {
+			list.add((int)Math.pow(2,i));
 		}
+		list.add((int)Math.pow(2,31)-1);
+		System.out.println(list);
+		gogo(list, n);
+		int answer = 0;
 		
 		return answer;
 	}
-
-	private int commonDivisorCount(int number){
-		int answer = 1;
-		int part = 0;
-		
-		for(int i = 2; i < Math.sqrt(number) + 1; i++){
-			if(number % i == 0){
-				part = 0;
-				while(number%i == 0){
-					number /= i;
-					part++;	
-				}
-				answer *= (part+1);
-			}
+	
+	private void gogo(ArrayList<Integer> list, int target) {
+		int getIndex = binarySearch(list, target);
+		if(list.get(getIndex) == target) {
+			System.out.println("the answer is : " +getIndex);
+			return;
 		}
-		
-		return answer;
+		System.out.println("target : "+target +"  and its index : " +getIndex);
+	}
+	
+	private int binarySearch(ArrayList<Integer> list, int target) {
+		int low = 0, high = list.size()-1;
+		int middle = (low + high)/2;
+		while(low < high) {
+			if(list.get(middle) == target) {
+				return middle;
+			}
+			if(list.get(middle) < target) {
+				low = middle + 1;
+			}else {
+				high = middle - 1;
+			}
+			middle = (low + high)/2;
+		}
+		return middle;
+	}
+	
+	public static void main(String[] args) {
+		IntegerReplacement s = new IntegerReplacement();
+		System.out.println(s.integerReplacement(4));
+		System.out.println(s.integerReplacement(513));
+		System.out.println(s.integerReplacement(1024));
+		System.out.println(s.integerReplacement(7));
 	}
 }

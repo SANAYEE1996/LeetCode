0b163e7fa9e140c24ae0688aad72eacabdce19ff
SANAYEE1996 <dudtkd0219@gmail.com>
2023-06-01 15:38:27 +0900

    18 ms, faster than 56.61% // 45.1 MB, less than 12.16%

--- src/medium/ShortestPathBinaryMatrix.java
+++ src/medium/ShortestPathBinaryMatrix.java
@@ -1,57 +1,59 @@
 package medium;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ShortestPathBinaryMatrix {
 	
 	/**
 	 * https://leetcode.com/problems/shortest-path-in-binary-matrix/
 	 * */
 	
-	private int count;
 
 	public int shortestPathBinaryMatrix(int[][] grid) {
-		count = Integer.MAX_VALUE;
-        boolean[] visited = new boolean[grid.length*grid[0].length];
-        if(grid[0][0] == 1 || grid[grid.length-1][grid[0].length-1] == 1){
-            return -1;
-        }
-        gogo(grid,0,0,visited,1);
-        if(count == Integer.MAX_VALUE){
-            return -1;
-        }
-        return count;
+		int[] dx = {0,1,0,-1,1,1,-1,-1};
+		int[] dy = {-1,0,1,0,1,-1,1,-1};
+		int m = grid.length;
+		int n = grid[0].length;
+		if(grid[0][0] == 1 || grid[m-1][n-1] == 1) {
+			return -1;
+		}
+		boolean[][] visited = new boolean[m][n];
+        return bfs(0,0,grid,visited,dx,dy,m,n);
 	}
 
-	private void gogo(int[][] grid, int i, int j, boolean[] visited, int way){
-        if(i == grid.length-1 && j == grid[i].length-1 && grid[i][j] == 0){
-            count = Math.min(count, way);
-            return;
-        }
-        int m = grid.length;
-        int n = grid[0].length;
-        visited[i*m+j] = true;
-        if(i > 0 && !visited[(i-1)*m + j] && grid[i-1][j] == 0){
-            gogo(grid, i-1, j, visited.clone(), way+1);
-        }
-        if(j > 0 && !visited[i*m + j-1] && grid[i][j-1] == 0){
-            gogo(grid, i, j-1, visited.clone(), way+1);
-        }
-        if(i < m-1 && !visited[(i+1)*m + j] && grid[i+1][j] == 0){
-            gogo(grid, i+1, j, visited.clone(), way+1);
-        }
-        if(j < n-1 && !visited[i*m + j+1] && grid[i][j+1] == 0){
-            gogo(grid, i, j+1, visited.clone(), way+1);
-        }
-        if(i > 0 && j > 0 && !visited[(i-1)*m + j-1] && grid[i-1][j-1] == 0){
-            gogo(grid, i-1, j-1, visited.clone(), way+1);
-        }
-        if(i > 0 && j < n-1 && !visited[(i-1)*m + j+1] && grid[i-1][j+1] == 0){
-            gogo(grid, i-1, j+1, visited.clone(), way+1);
-        }
-        if(i < m-1 && j > 0 && !visited[(i+1)*m + j-1] && grid[i+1][j-1] == 0){
-            gogo(grid, i+1, j-1, visited.clone(), way+1);
-        }
-        if(i < m-1 && j < n-1 && !visited[(i+1)*m + j+1] && grid[i+1][j+1] == 0){
-            gogo(grid, i+1, j+1, visited.clone(), way+1);
-        }
-    }
+	private int bfs(int x, int y, int[][] maps, boolean[][] visited, int[] dx, int[] dy, int m, int n) {
+		Queue<Node> q = new LinkedList<>();
+		q.offer(new Node(x, y, 1));
+		visited[x][y] = true;
+		
+		while (!q.isEmpty()) {
+			Node node = q.poll();
+			if (node.x == m - 1 && node.y == n - 1)
+				return node.way;
+
+			for (int i = 0; i < 8; i++) {
+				int nx = node.x + dx[i];
+				int ny = node.y + dy[i];
+				if (nx >= 0 && ny >= 0 && nx < m && ny < n) {
+					if (maps[nx][ny] == 0 && !visited[nx][ny]) {
+						visited[nx][ny] = true;
+						q.offer(new Node(nx, ny, node.way + 1));
+					}
+				}
+			}
+		}
+		return -1;
+	}
+}
+
+class Node{
+	int x;
+	int y;
+	int way;
+	public Node(int x, int y, int way){
+		this.x = x;
+		this.y = y;
+		this.way = way;
+	}
 }

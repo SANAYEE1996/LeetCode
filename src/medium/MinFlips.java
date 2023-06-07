cf4761f6912b264cf1abe21be450c862071fec29
SANAYEE1996 <dudtkd0219@gmail.com>
2023-06-07 16:23:12 +0900

    0 ms, faster than 100.00% // 39.1 MB, less than 95.42%

---
+++ src/medium/MinFlips.java
@@ -0,0 +1,27 @@
+package medium;
+
+public class MinFlips {
+	
+	/**
+	 * https://leetcode.com/problems/minimum-flips-to-make-a-or-b-equal-to-c/solution/
+	 * */
+	
+	public int minFlips(int a, int b, int c) {
+		int answer = 0;
+		while (a != 0 | b != 0 | c != 0) {
+			if ((c & 1) == 1) {
+				if ((a & 1) == 0 && (b & 1) == 0) {
+					answer++;
+				}
+			} else {
+				answer += (a & 1) + (b & 1);
+			}
+			
+			a >>= 1;
+			b >>= 1;
+			c >>= 1;
+		}
+		
+		return answer;
+	}  
+}

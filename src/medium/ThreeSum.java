e88db27f99f81dec5c2c4c83a87ce8949ee2875a
SANAYEE1996 <dudtkd0219@gmail.com>
2023-06-23 16:56:39 +0900

    87 ms, faster than 43.31% // 53.4 MB, less than 5.21%

--- src/medium/ThreeSum.java
+++ src/medium/ThreeSum.java
@@ -54,14 +54,4 @@ public class ThreeSum {
 		answerList.addAll(answerSet);
 		return answerList;
 	}
-	
-	
-	public static void main(String[] args) {
-		ThreeSum s = new ThreeSum();
-		
-		System.out.println(s.threeSum(new int[] {-1,0,1,2,-1,-4}));
-		System.out.println(s.threeSum(new int[] {0,1,1}));
-		System.out.println(s.threeSum(new int[] {0,0,0}));
-	}
-	
 }

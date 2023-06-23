3fe19f34150a351ebd544c1343911e933d344945
SANAYEE1996 <dudtkd0219@gmail.com>
2023-06-23 16:56:19 +0900

    87 ms, faster than 43.31% // 53.4 MB, less than 5.21%

--- src/medium/ThreeSum.java
+++ src/medium/ThreeSum.java
@@ -1,102 +1,60 @@
 package medium;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ThreeSum {
 	public List<List<Integer>> threeSum(int[] nums) {
+		Arrays.sort(nums);
+		List<Integer> minusList = new ArrayList<>();
+		List<Integer> zeroList = new ArrayList<>();
+		List<Integer> plusList = new ArrayList<>();
+		Set<Integer> minusSet = new HashSet<>();
+		Set<Integer> zeroSet = new HashSet<>();
+		Set<Integer> plusSet = new HashSet<>();
+		for(int i : nums){
+			if(0 < i) {plusList.add(i); plusSet.add(i);}
+			if(i == 0) {zeroList.add(i); zeroSet.add(i);}
+			if(i < 0) {minusList.add(i); minusSet.add(i);}
+		}
+		Set<List<Integer>> answerSet = new HashSet<>();
+		int maxPlusValue = !plusList.isEmpty() ? plusList.get(plusList.size()-1) : 0;
+		int minMinusValue = !minusList.isEmpty() ? minusList.get(0) : 0;
+		for(int i = 0; i < minusList.size()-1; i++){
+			int max = minusList.get(i);
+			for(int j = minusList.size()-1; j > i; j--){
+				int min = minusList.get(j);
+				int absValue = Math.abs(max+min);
+				if(absValue > maxPlusValue) break;
+				if(!plusSet.contains(absValue)) continue;
+				answerSet.add(Arrays.asList(new Integer[]{max,min,absValue}));
+			}
+		}
+		for(int i = plusList.size()-1; i > 0; i--){
+			int max = plusList.get(i);
+			for(int j = 0; j < i; j++){
+				int min = plusList.get(j);
+				int absValue = -(max+min);
+				if(absValue < minMinusValue) break;
+				if(!minusSet.contains(absValue)) continue;
+				answerSet.add(Arrays.asList(new Integer[]{absValue,min,max}));
+			}
+		}
+		if(!zeroList.isEmpty()){
+			for(int i : minusList){
+				if(!plusSet.contains(-i)) continue;
+				answerSet.add(Arrays.asList(new Integer[]{i,0,-i}));
+			}
+		}
+		if(zeroList.size() > 2) answerSet.add(Arrays.asList(new Integer[]{0,0,0}));
 		List<List<Integer>> answerList = new ArrayList<>();
-		HashMap<Integer, Integer> plusMap = new HashMap<>();
-		HashMap<Integer, Integer> minusMap = new HashMap<>();
-		HashMap<Integer, Integer> zeroMap = new HashMap<>();
-		
-		for(int i : nums) {
-			if(i < 0) {
-				putValueToMap(minusMap, i);
-			}
-			else if(i > 0) {
-				putValueToMap(plusMap, i);
-			}
-			else {
-				putValueToMap(zeroMap, i);
-			}
-		}
-		
-		int twoSum = 0;
-		
-		for(int plusKey : plusMap.keySet()) {
-			for(int minusKey : minusMap.keySet()) {
-				twoSum = plusKey + minusKey;
-				if(twoSum < 0) {
-					putValueToList(plusMap, twoSum, plusKey, minusKey, answerList);
-				}
-				else if(twoSum > 0) {
-					putValueToList(minusMap, twoSum, minusKey, plusKey, answerList);
-				}
-				else {
-					if(!zeroMap.isEmpty()) {
-						ArrayList<Integer> examList = new ArrayList<>();
-						examList.add(plusKey);
-						examList.add(0);
-						examList.add(minusKey);
-						if(!answerList.contains(examList)) {
-							answerList.add(examList);
-						}
-					}
-				}
-			}
-		}
-		
-		if(!zeroMap.isEmpty() && zeroMap.get(0) > 2) {
-			ArrayList<Integer> examList = new ArrayList<>();
-			examList.add(0);
-			examList.add(0);
-			examList.add(0);
-			answerList.add(examList);
-		}
-		
+		answerList.addAll(answerSet);
 		return answerList;
-    }
-	
-	private void putValueToMap(HashMap<Integer, Integer> map, int value) {
-		if(!map.containsKey(value)) {
-			map.put(value, 0);
-		}
-		map.put(value, map.get(value)+1);
 	}
 	
-	private void putValueToList(HashMap<Integer, Integer> map, int sum, int compareKey, int putKey, List<List<Integer>> answerList) {
-		if(map.containsKey(-sum)) {
-			if(-sum == compareKey && map.get(compareKey) < 2) {
-				return;
-			}
-			ArrayList<Integer> examList = new ArrayList<>();
-			int secondValue = 0;
-			if(compareKey > putKey) {
-				if(putKey > -sum) {
-					secondValue = putKey;
-				}
-				else {
-					secondValue = Math.min(compareKey, -sum);
-				}
-			}
-			else { //compareKey <= putKey
-				if(-sum < compareKey) {
-					secondValue = compareKey;
-				}
-				else { //-sum >= compareKey
-					secondValue = Math.min(putKey, -sum);
-				}
-			}
-			examList.add(Math.max(compareKey, Math.max(-sum, putKey)));
-			examList.add(secondValue);
-			examList.add(Math.min(compareKey, Math.min(-sum, putKey)));
-			if(!answerList.contains(examList)) {
-				answerList.add(examList);
-			}
-		}
-	}
 	
 	public static void main(String[] args) {
 		ThreeSum s = new ThreeSum();

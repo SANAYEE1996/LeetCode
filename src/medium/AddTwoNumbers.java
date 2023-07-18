8cabb3d84f53dd25af32e575709d761f7db8b864
SANAYEE1996 <dudtkd0219@gmail.com>
2023-07-18 15:47:54 +0900

    Runtime: 6 ms // Memory Usage: 44.2 MB

--- src/medium/AddTwoNumbers.java
+++ src/medium/AddTwoNumbers.java
@@ -1,39 +1,40 @@
 package medium;
 
-public class AddTwoNumbers {
-	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-        int carry = 0;
-        while(l1 != null || l2 != null || carry == 1){
-            int sum = 0;
-            if(l1 != null){
-                sum += l1.val;
-                l1 = l1.next;
-            }
-            if(l2 != null){
-                sum += l2.val;
-                l2 = l2.next;
-            }
-            sum += carry;
-            carry = sum/10;
-            ListNode node = new ListNode(sum % 10);
-            curr.next = node;
-            curr = curr.next;
-        }
-        return dummy.next;
-    }
-	
-	void printListNode(ListNode l1) {
-		ListNode exam01 = l1;
-		while(exam01 != null) {
-			System.out.print(exam01.val + " ");
-			exam01 = exam01.next;
-		}
-	}
-	
-	public static void main(String[] args) {
+import java.util.Stack;
 
-		
-	}
+public class AddTwoNumbers {
+	
+	/**
+	 * https://leetcode.com/problems/add-two-numbers-ii/
+	 * */
+	
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> s1 = new Stack<Integer>();
+        Stack<Integer> s2 = new Stack<Integer>();
+        
+        while(l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        };
+        while(l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+        
+        int totalSum = 0, carry = 0;
+        ListNode ans = new ListNode();
+        while (!s1.empty() || !s2.empty()) {
+            if (!s1.empty()) totalSum += s1.pop();
+            if (!s2.empty()) totalSum += s2.pop();
+            
+            ans.val = totalSum % 10;
+            carry = totalSum / 10;
+            ListNode head = new ListNode(carry);
+            head.next = ans;
+            ans = head;
+            totalSum = carry;
+        }
+
+        return carry == 0 ? ans.next: ans;
+    }
 }

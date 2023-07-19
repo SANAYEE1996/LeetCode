3d9eebc5c79c48c103b115e13245fb71fa47de7f
SANAYEE <dudtkd0219@gmail.com>
2023-07-19 21:14:13 +0900

    Runtime: 80 ms Memory Usage: 127.9 MB

--- src/medium/LRUCache.java
+++ src/medium/LRUCache.java
@@ -1,70 +1,62 @@
 package medium;
 
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.Map;
 
 public class LRUCache {
 	
-	private HashMap<Integer, Integer> map;
-	private HashMap<Integer, Integer> keyIndexMap;
-	private LinkedList<Integer> recentKeyList;
-	private int capacity;
+	Node head = new Node(0, 0), tail = new Node(0, 0);
+	  Map<Integer, Node> map = new HashMap<>();
+	  int capacity;
+	  
+	  public LRUCache(int _capacity) {
+	    capacity = _capacity;
+	    head.next = tail;
+	    tail.prev = head;
+	  }
 
-	public LRUCache(int capacity) {
-		map = new HashMap<>();
-		keyIndexMap = new HashMap<>();
-		recentKeyList = new LinkedList<>();
-		this.capacity = capacity;
-	}
+	  public int get(int key) {
+	    if(map.containsKey(key)) {
+	      Node node = map.get(key);
+	      remove(node);
+	      insert(node);
+	      return node.value;
+	    } else {
+	      return -1;
+	    }
+	  }
 
-	public int get(int key) {
-		if(!map.containsKey(key)){
-			return -1;
-		}
-		updateMap(key);
-		return map.get(key);
-	}
-
-	public void put(int key, int value) {
-		if(map.containsKey(key)){
-			map.put(key,value);
-			updateMap(key);
-			return;
-		}
-		if(map.size() == capacity){
-			map.put(key,value);
-			lruActive(key);
-			return;
-		}
-		map.put(key,value);
-		updateMap(key);
-	}
-	
-	private void updateMap(int key) {
-		if(keyIndexMap.containsKey(key)){
-			int index = keyIndexMap.get(key);
-			recentKeyList.remove(index);
-			recentKeyList.add(key);
-			int lastIndex = recentKeyList.size()-1;
-			keyIndexMap.put(key,lastIndex);
-		}
-		else{
-			recentKeyList.add(key);
-			int lastIndex = recentKeyList.size()-1;
-			keyIndexMap.put(key,lastIndex);
-		}
-	}
-	
-	private void lruActive(int key) {
-		int mapKey = recentKeyList.get(0);
-		keyIndexMap.remove(mapKey);
-		for(int keyIndexMapKey : keyIndexMap.keySet()){
-			keyIndexMap.put(keyIndexMapKey, keyIndexMap.get(keyIndexMapKey)-1);
-		}
-		map.remove(mapKey);
-		recentKeyList.remove(0);
-		recentKeyList.add(key);
-		int lastIndx = recentKeyList.size()-1;
-		keyIndexMap.put(key,lastIndx);
-	}
+	  public void put(int key, int value) {
+	    if(map.containsKey(key)) {
+	      remove(map.get(key));
+	    }
+	    if(map.size() == capacity) {
+	      remove(tail.prev);
+	    }
+	    insert(new Node(key, value));
+	  }
+	  
+	  private void remove(Node node) {
+	    map.remove(node.key);
+	    node.prev.next = node.next;
+	    node.next.prev = node.prev;
+	  }
+	  
+	  private void insert(Node node){
+	    map.put(node.key, node);
+	    Node headNext = head.next;
+	    head.next = node;
+	    node.prev = head;
+	    headNext.prev = node;
+	    node.next = headNext;
+	  }
+	  
+	  class Node{
+	    Node prev, next;
+	    int key, value;
+	    Node(int _key, int _value) {
+	      key = _key;
+	      value = _value;
+	    }
+	  }
 }

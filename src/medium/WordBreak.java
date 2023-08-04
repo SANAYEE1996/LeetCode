b129182ca695a68c54c69412ad6f4a325927e2c5
SANAYEE1996 <dudtkd0219@gmail.com>
2023-08-04 14:28:00 +0900

    time limit exceeded

--- src/medium/WordBreak.java
+++ src/medium/WordBreak.java
@@ -1,58 +1,34 @@
 package medium;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WordBreak {
 	
+	/**
+	 * https://leetcode.com/problems/word-break/
+	 * */
+	
+	private boolean isBreakable;
 	
 	public boolean wordBreak(String s, List<String> wordDict) {
-		
-		if (!isStartWithWordDictList(s, wordDict)) {
-			return false;
+		Set<String> set = new HashSet<>();
+		set.addAll(wordDict);
+		isBreakable = false;
+		gogo(s, set);
+		return isBreakable;
+	}
+
+	private void gogo(String s, Set<String> set){
+		if(s.length() == 0){
+			isBreakable = true;
+			return;
 		}
-		
-		
-		return true;
-    }
-	
-	private boolean isStartWithWordDictList(String word, List<String> wordDict) {
-		
-		if("".equals(word)) {
-			return true;
-		}
-		
-		for(String str : wordDict) {
-			if(word.startsWith(str)) {
-				if (isStartWithWordDictList(word.substring(str.length()), wordDict)) {
-					return true;
-				}
+		for(int i = 1; i <= s.length(); i++){
+			if(set.contains(s.substring(0,i))){
+				gogo(s.substring(i), set);
 			}
 		}
-		
-		return false;
 	}
-	
-	public static void main(String[] args) {
-		WordBreak s = new WordBreak();
-		List<String> wordDict = new ArrayList<>(Arrays.asList(new String[] {"cats","dog","sand","and","cat"}));
-		System.out.println(s.wordBreak("catsandog", wordDict));
-		wordDict = new ArrayList<>(Arrays.asList(new String[] {"apple","pen"}));
-		System.out.println(s.wordBreak("applepenapple", wordDict));
-		wordDict = new ArrayList<>(Arrays.asList(new String[] {"leet","code"}));
-		System.out.println(s.wordBreak("leetcode", wordDict));
-		
-		String[] strArray = {"abbcbda","cbdaaa","b","dadaaad","dccbbbc","dccadd","ccbdbc","bbca","bacbcdd",
-				 "a","bacb","cbc","adc","c","cbdbcad","cdbab","db","abbcdbd","bcb","bbdab",
-				 "aa","bcadb","bacbcb","ca","dbdabdb","ccd","acbb","bdc","acbccd","d",
-				 "cccdcda","dcbd","cbccacd","ac","cca","aaddc","dccac","ccdc","bbbbcda",
-				 "ba","adbcadb","dca","abd","bdbb","ddadbad","badb","ab","aaaaa","acba","abbb"};
-		wordDict = new ArrayList<>(Arrays.asList(strArray));
-		System.out.println(s.wordBreak("acaaaaabbbdbcccdcdaadcdccacbcccabbbbcdaaaaaadb", wordDict));
-		
-		wordDict = new ArrayList<>(Arrays.asList(new String[] {"cats","dog","sand","cat"}));
-		System.out.println(s.wordBreak("catsanddog", wordDict));
-	}
-	
 }

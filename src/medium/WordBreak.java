70376b20e1c6e9c7fc668905bdc4d3c4991b4d11
SANAYEE1996 <dudtkd0219@gmail.com>
2023-08-07 12:14:41 +0900

    fail

--- src/medium/WordBreak.java
+++ src/medium/WordBreak.java
@@ -1,8 +1,8 @@
 package medium;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class WordBreak {
 	
@@ -10,25 +10,24 @@ public class WordBreak {
 	 * https://leetcode.com/problems/word-break/
 	 * */
 	
-	private boolean isBreakable;
-	
 	public boolean wordBreak(String s, List<String> wordDict) {
-		Set<String> set = new HashSet<>();
-		set.addAll(wordDict);
-		isBreakable = false;
-		gogo(s, set);
-		return isBreakable;
-	}
-
-	private void gogo(String s, Set<String> set){
-		if(s.length() == 0){
-			isBreakable = true;
-			return;
-		}
-		for(int i = 1; i <= s.length(); i++){
-			if(set.contains(s.substring(0,i))){
-				gogo(s.substring(i), set);
-			}
-		}
-	}
+        return helper(s,new HashSet<>(wordDict),new HashMap<String, Boolean>());
+    }
+	
+    private boolean helper(String s, HashSet<String>dict,HashMap<String,Boolean>dp){
+        if(s.length()==0)return true;
+        if(dict.contains(s))return true;
+        
+        if(dp.containsKey(s))return dp.get(s);
+        
+        for(int i=1;i<s.length();i++){
+            String subs = s.substring(0,i);
+            if(dict.contains(subs)&&helper(s.substring(i),dict,dp)){
+                dp.put(subs,true);
+                return true;
+            }
+        }
+        dp.put(s,false);
+        return false;
+    }
 }

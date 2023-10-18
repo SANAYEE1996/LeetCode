e8f84b25a57bd916c4a56c00c278d076d966afe7
SANAYEE1996 <dudtkd0219@gmail.com>
2023-10-18 09:56:24 +0900

    test code add

---
+++ src/medium/test/ConstructProductMatrixTest.java
@@ -0,0 +1,37 @@
+package medium.test;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import medium.ConstructProductMatrix;
+
+class ConstructProductMatrixTest {
+	
+	private ConstructProductMatrix cpm;
+	
+	@BeforeEach
+	void init() {
+		cpm = new ConstructProductMatrix();
+	}
+
+	@Test
+	void test01() {
+		int[][] grid = {{1,2},{3,4}};
+		int[][] result = cpm.constructProductMatrix(grid);
+		for(int[] i : result) {
+			System.out.println(Arrays.toString(i));
+		}
+	}
+	
+	@Test
+	void test02() {
+		int[][] grid = {{12345},{2},{1}};
+		int[][] result = cpm.constructProductMatrix(grid);
+		for(int[] i : result) {
+			System.out.println(Arrays.toString(i));
+		}
+	}
+
+}

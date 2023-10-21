3e9d3c5d802dbaf6d116e640ff939293b4084e4a
SANAYEE <dudtkd0219@gmail.com>
2023-10-21 15:53:13 +0900

    init commit

---
+++ src/medium/MaximumSumOfHeights2.java
@@ -0,0 +1,15 @@
+package medium;
+
+import java.util.List;
+
+public class MaximumSumOfHeights2 {
+	
+	/**
+	 * https://leetcode.com/problems/beautiful-towers-ii/
+	 * */
+	
+	public long maximumSumOfHeights(List<Integer> maxHeights) {
+        
+		return 0;
+    }
+}

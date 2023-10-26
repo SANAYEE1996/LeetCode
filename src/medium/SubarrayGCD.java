239c27665a787d393d40d0f70ccd3f10d1ea8761
SANAYEE1996 <dudtkd0219@gmail.com>
2023-10-26 14:05:33 +0900

    again try

--- src/medium/SubarrayGCD.java
+++ src/medium/SubarrayGCD.java
@@ -1,35 +1,25 @@
 package medium;
 
-import java.util.ArrayList;
-
 public class SubarrayGCD {
 	public int subarrayGCD(int[] nums, int k) {
 		int answer = 0;
-        for(int i = 0; i < nums.length; i++) {
-        	if(nums[i] % k == 0) {
-        		ArrayList<Integer> multipleList = new ArrayList<>();
-        		for(; i < nums.length; i++) {
-        			if(nums[i] % k != 0) {
-        				break;
-        			}
-        			multipleList.add(nums[i]/k);
-        		}
-        		answer += getCaseCount(multipleList);
-//        		System.out.println("multipleList : " +multipleList);
-//        		System.out.println("is answer ? : " +answer);
-        	}
-        }
-//		System.out.println("last answer : " +answer);
-		return answer;
-    }
-	
-	private int getCaseCount(ArrayList<Integer> list) {
-		int count = 0;
-		for(int i = 0; i < list.size(); i++) {
-			if(list.get(i) == 1) {
-				count += list.size();
+		for(int i = 0; i < nums.length; i++){
+			nums[i] = (nums[i]%k == 0) ? ((nums[i] == k) ? 0 : 1) : -1;
+		}
+		int oneCount = 0, zeroCount = 0;
+		for(int i = 0; i < nums.length; i++){
+			if(nums[i] == 0){
+				zeroCount++;
+			}
+			else if(nums[i] == 1){
+				oneCount++;
+			}
+			else{
+				oneCount = 0;
+				zeroCount = 0;
 			}
 		}
-		return count;
-	}
+		
+		return answer;
+    }
 }
